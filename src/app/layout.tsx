b883c5851bbbfd8bd93dfb0898fbae301aa8dfae
sbvhev <rohannepal200@gmail.com>
2024-03-26 13:08:13 -0400

    Add next auth to the router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import AppProvider from "@/providers/AppProvider"
+import NextAuthProvider from "@/providers/NextAuthProvider"
 import Header from "@/components/Header"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -19,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppProvider>
-          <Header />
-          {children}
-        </AppProvider>
+        <NextAuthProvider>
+          <AppProvider>
+            <Header />
+            {children}
+          </AppProvider>
+        </NextAuthProvider>
       </body>
     </html>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 "use client"
-import { SignInButton } from "@farcaster/auth-kit"
+import { useSignIn } from "@farcaster/auth-kit"
 
 const Header = () => {
+  const res = useSignIn({
+    onSuccess: ({ fid }) => console.log("Your fid:", fid),
+  })
+
+  console.log("res: ", res)
+
   return (
     <div id="header" className="flex items-center justify-between w-full p-4">
       <div className="flex gap-2">
         <div className="flex items-center gap-2 px-3 text-white bg-black">
-          <SignInButton />
+          <button onClick={res.connect}>Sign In</button>
         </div>
       </div>
     </div>
