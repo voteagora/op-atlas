5f98582606360d2b3cbc8f5fc966a1cac0fa3051
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-04-29 18:24:53 -0400

    fix copy

--- src/components/projects/repos/NpmPackageManager.tsx
+++ src/components/projects/repos/NpmPackageManager.tsx
@@ -4,15 +4,10 @@ import { Input } from "@/components/ui/input"
 const NpmPackageManager = () => {
   return (
     <div className="flex flex-col gap-6">
-      <h3 className=" text-lg font-semibold">NPM</h3>
-      <div>
-        <p className=" text-sm font-medium">
-          Enter your node package manager URL
-        </p>
-        <p className=" text-sm font-normal text-secondary-foreground">
-          If you have multiple repos, first verify one then you can add more.
-        </p>
-        <Input type="text" placeholder="Add a URL" className="w-full mt-2" />
+      <h3 className="text-lg font-semibold">NPM</h3>
+      <div className="flex flex-col gap-2">
+        <p className="text-sm font-medium">Node package manager</p>
+        <Input type="text" placeholder="Add a URL" className="w-full" />
       </div>
     </div>
   )
