0a1073968555569b4989738c14ae05ea49a7503a
Brian Wagner <bhw2113@columbia.edu>
2024-05-13 18:31:28 -0400

    Add RPC urls for chains (#66)

--- src/lib/eth.ts
+++ src/lib/eth.ts
@@ -3,13 +3,36 @@ import { base, fraxtal, mainnet, mode, optimism, zora } from "viem/chains"
 
 import { Chain } from "./utils/contracts"
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+if (!ALCHEMY_API_KEY) {
+  throw new Error("ALCHEMY_API_KEY is missing from env")
+}
+
 const clients = {
-  [Chain.Eth]: createPublicClient({ chain: mainnet, transport: http() }),
-  [Chain.Base]: createPublicClient({ chain: base, transport: http() }),
-  [Chain.Fraxtal]: createPublicClient({ chain: fraxtal, transport: http() }),
-  [Chain.Mode]: createPublicClient({ chain: mode, transport: http() }),
-  [Chain.Optimism]: createPublicClient({ chain: optimism, transport: http() }),
-  [Chain.Zora]: createPublicClient({ chain: zora, transport: http() }),
+  [Chain.Eth]: createPublicClient({
+    chain: mainnet,
+    transport: http(`https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`),
+  }),
+  [Chain.Base]: createPublicClient({
+    chain: base,
+    transport: http(`https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`),
+  }),
+  [Chain.Fraxtal]: createPublicClient({
+    chain: fraxtal,
+    transport: http("https://rpc.frax.com"),
+  }),
+  [Chain.Mode]: createPublicClient({
+    chain: mode,
+    transport: http("https://mainnet.mode.network"),
+  }),
+  [Chain.Optimism]: createPublicClient({
+    chain: optimism,
+    transport: http(`https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`),
+  }),
+  [Chain.Zora]: createPublicClient({
+    chain: zora,
+    transport: http("https://rpc.zora.energy"),
+  }),
 }
 
 export async function getTransaction(hash: `0x${string}`, chain: Chain) {
