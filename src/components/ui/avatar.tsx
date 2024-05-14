d448bcba1fd4aee1e0035f7ba62bb7d63ba8f03a
Brian Wagner <brian.wagner35@gmail.com>
2024-05-14 12:31:53 -0400

    Fix avatar stretching

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -26,7 +26,10 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn(
+      "aspect-square object-cover object-center h-full w-full",
+      className,
+    )}
     {...props}
   />
 ))
