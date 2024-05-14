479af780feaf62550df7966996991cce307ce2eb
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-05-14 12:30:07 -0400

    add confetti

--- src/components/application/ApplicationSubmitted.tsx
+++ src/components/application/ApplicationSubmitted.tsx
@@ -3,7 +3,8 @@ import { ArrowDownToLine } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { useSession } from "next-auth/react"
-import { useLayoutEffect } from "react"
+import { useLayoutEffect, useState } from "react"
+import Confetti from "react-dom-confetti"
 
 import { cn } from "@/lib/utils"
 
@@ -26,6 +27,20 @@ const SOCIALS = [
   },
 ] as const
 
+const confettiConfig = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: 70,
+  dragFriction: 0.12,
+  duration: 3000,
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+}
+
 export const ApplicationSubmitted = ({
   className,
   application,
@@ -34,9 +49,11 @@ export const ApplicationSubmitted = ({
   application: Application
 }) => {
   const { data: session } = useSession()
+  const [showConfetti, setShowConfetti] = useState(false)
 
   // Scroll to top on mount
   useLayoutEffect(() => {
+    setShowConfetti(true)
     window.scrollTo(0, 0)
   }, [])
 
@@ -51,6 +68,8 @@ export const ApplicationSubmitted = ({
     >
       {/* Header */}
       <div className="flex flex-col items-center gap-y-6">
+        <Confetti active={showConfetti} config={confettiConfig} />
+
         <Image
           alt="sunny"
           src="/assets/images/round-4-transparent.svg"
