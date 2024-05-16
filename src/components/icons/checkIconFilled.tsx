5e741105e7505391878323a1d2d18f415822f689
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-05-15 22:19:10 -0400

    add colored icons

--- src/components/icons/checkIconFilled.tsx
+++ src/components/icons/checkIconFilled.tsx
@@ -1,8 +1,14 @@
-export const CheckIconFilled = ({ className }: { className?: string }) => {
+export const CheckIconFilled = ({
+  className,
+  size = 24,
+}: {
+  className?: string
+  size?: number
+}) => {
   return (
     <svg
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
