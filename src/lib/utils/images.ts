e70156a1c20beea3c217bf799c50291d2954bd51
Tina <59578595+tinaszheng@users.noreply.github.com>
2024-05-22 18:41:40 -0400

    fix: photo sizing issue (#112)

--- src/lib/utils/images.ts
+++ src/lib/utils/images.ts
@@ -1,6 +1,5 @@
+import imageCompression from "browser-image-compression"
 import { PixelCrop } from "react-image-crop"
-
-const TO_RADIANS = Math.PI / 180
 
 export function toBlob(canvas: HTMLCanvasElement): Promise<Blob | null> {
   return new Promise((resolve) => {
@@ -9,9 +8,11 @@ export function toBlob(canvas: HTMLCanvasElement): Promise<Blob | null> {
 }
 
 export async function uploadImage(image: Blob) {
+  const compressed = await compressImage(image)
+
   const result = await fetch("/api/upload", {
     method: "POST",
-    body: image,
+    body: compressed,
   })
 
   if (result.status === 413) {
@@ -28,8 +29,6 @@ export async function canvasPreview(
   image: HTMLImageElement,
   canvas: HTMLCanvasElement,
   crop: PixelCrop,
-  scale = 1,
-  rotate = 0,
 ) {
   const ctx = canvas.getContext("2d")
 
@@ -39,12 +38,8 @@ export async function canvasPreview(
 
   const scaleX = image.naturalWidth / image.width
   const scaleY = image.naturalHeight / image.height
-  // devicePixelRatio slightly increases sharpness on retina devices
-  // at the expense of slightly slower render times and needing to
-  // size the image back down if you want to download/upload and be
-  // true to the images natural size.
+
   const pixelRatio = window.devicePixelRatio
-  // const pixelRatio = 1
 
   canvas.width = Math.floor(crop.width * scaleX * pixelRatio)
   canvas.height = Math.floor(crop.height * scaleY * pixelRatio)
@@ -55,7 +50,6 @@ export async function canvasPreview(
   const cropX = crop.x * scaleX
   const cropY = crop.y * scaleY
 
-  const rotateRads = rotate * TO_RADIANS
   const centerX = image.naturalWidth / 2
   const centerY = image.naturalHeight / 2
 
@@ -65,10 +59,6 @@ export async function canvasPreview(
   ctx.translate(-cropX, -cropY)
   // 4) Move the origin to the center of the original position
   ctx.translate(centerX, centerY)
-  // 3) Rotate around the origin
-  ctx.rotate(rotateRads)
-  // 2) Scale the image
-  ctx.scale(scale, scale)
   // 1) Move the center of the image to the origin (0,0)
   ctx.translate(-centerX, -centerY)
   ctx.drawImage(
@@ -84,4 +74,14 @@ export async function canvasPreview(
   )
 
   ctx.restore()
+}
+
+export async function compressImage(image: Blob) {
+  const options = {
+    maxSizeMB: 4,
+    maxWidthOrHeight: 4096,
+    useWebWorker: true,
+  }
+
+  return await imageCompression(image as File, options)
 }
