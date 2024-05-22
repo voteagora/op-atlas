a4d6565d95597a6dedddde88ba7a3f94fffd546d
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-05-22 12:58:52 -0400

    use lg shadow

--- src/components/common/FeedbackButton.tsx
+++ src/components/common/FeedbackButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button"
 export function FeedbackButton() {
   return (
     <ExternalLink href="https://ztfatrpxjq1.typeform.com/to/pBEaxkMN">
-      <Button variant="outline" className="shadow-md">
+      <Button variant="outline" className="shadow-lg">
         Give feedback
       </Button>
     </ExternalLink>
