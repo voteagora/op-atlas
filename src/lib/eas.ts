e5360e42dc53fc04124b44343329500b8827c200
Brian Wagner <bhw2113@columbia.edu>
2024-05-22 11:26:47 -0700

    Update project attestation schema (#101)

--- src/lib/eas.ts
+++ src/lib/eas.ts
@@ -2,13 +2,13 @@ import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk"
 import { ethers, Wallet } from "ethers"
 
 const PROJECT_SCHEMA_ID =
-  "0x0e5d439a46d50507c63ea277b75c4d87711cc9d1754103393066927ee9be9fe3"
+  "0x7ae9f4adabd9214049df72f58eceffc48c4a69e920882f5b06a6c69a3157e5bd"
 const METADATA_SCHEMA_ID =
   "0xe035e3fe27a64c8d7291ae54c6e85676addcbc2d179224fe7fc1f7f05a8c6eac"
 const APPLICATION_SCHEMA_ID =
   "0x88b62595c76fbcd261710d0930b5f1cc2e56758e155dea537f82bf0baadd9a32"
 
-const projectSchema = new SchemaEncoder("uint256 farcasterID")
+const projectSchema = new SchemaEncoder("uint256 farcasterID,string issuer")
 const metadataSchema = new SchemaEncoder(
   "bytes32 projectRefUID,uint256 farcasterID,string name,string category,bytes32 parentProjectRefUID,uint8 metadataType,string metadataUrl",
 )
@@ -50,10 +50,11 @@ async function createAttestation(schemaId: string, data: string) {
 export async function createProjectAttestation({
   farcasterId,
 }: {
-  farcasterId: string
+  farcasterId: number
 }) {
   const data = projectSchema.encodeData([
     { name: "farcasterID", value: farcasterId, type: "uint256" },
+    { name: "issuer", value: "OP Atlas", type: "string" },
   ])
 
   const attestationId = await createAttestation(PROJECT_SCHEMA_ID, data)
