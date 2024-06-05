e5b66284888dff92cd612c49a3333e6b7a56f414
Brian Wagner <brian.wagner35@gmail.com>
2024-06-05 10:09:05 -0400

    Bump publish timeout as well

--- src/app/projects/[projectId]/publish/page.tsx
+++ src/app/projects/[projectId]/publish/page.tsx
@@ -5,7 +5,7 @@ import { PublishForm } from "@/components/projects/publish/PublishForm"
 import { getProject } from "@/db/projects"
 import { isUserMember } from "@/lib/actions/utils"
 
-export const maxDuration = 60
+export const maxDuration = 120
 
 export default async function Page({
   params,
