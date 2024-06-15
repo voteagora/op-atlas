646defaaa0b6e19abe286998d5acbe370efc59e3
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-06-15 15:09:51 -0400

    add profile details page

---
+++ src/app/profile/ProfileSidebar.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+
+export function ProfileSidebar() {
+  const pathname = usePathname()
+
+  const currentPage = pathname.split("/").slice(-1)[0]
+
+  return (
+    <div className="flex flex-col">
+      <div className="py-1 px-3 border-b border-border text-sm font-semibold">
+        Profile settings
+      </div>
+      <div className="flex flex-col gap-3 text-secondary-foreground text-sm py-2">
+        <Link
+          href="/profile/details"
+          className={cn(
+            currentPage === "details" && "text-foreground",
+            "flex gap-2 items-center",
+          )}
+        >
+          <div
+            className={cn(currentPage !== "details" && "invisible", "text-lg")}
+          >
+            •
+          </div>
+          Details
+        </Link>
+        <Link
+          href="/profile/connected-apps"
+          className={cn(
+            currentPage === "connected-apps" && "text-foreground",
+            "flex gap-2 items-center",
+          )}
+        >
+          <div
+            className={cn(
+              currentPage !== "connected-apps" && "invisible",
+              "text-lg",
+            )}
+          >
+            •
+          </div>
+          Connected apps
+        </Link>
+        <Link
+          href="/profile/verified-addresses"
+          className={cn(
+            currentPage === "verified-addresses" && "text-foreground",
+            "flex gap-2 items-center",
+          )}
+        >
+          <div
+            className={cn(
+              currentPage !== "verified-addresses" && "invisible",
+              "text-lg",
+            )}
+          >
+            •
+          </div>
+          Verified addresses
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/profile/details/page.tsx
@@ -0,0 +1,84 @@
+import { MoveUpRight } from "lucide-react"
+import { redirect } from "next/navigation"
+
+import { auth } from "@/auth"
+import ExternalLink from "@/components/ExternalLink"
+import { EditEmail } from "@/components/profile/EditEmail"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { getUserById } from "@/db/users"
+
+export default async function Page() {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    redirect("/")
+  }
+
+  const user = await getUserById(session.user.id)
+
+  if (!user) {
+    redirect("/")
+  }
+
+  return (
+    <div className="flex flex-col gap-12 text-secondary-foreground">
+      <div className="text-foreground text-2xl font-semibold">
+        Profile details
+      </div>
+      <EditEmail user={user} />
+      <div className="flex flex-col gap-6">
+        <div className="text-foreground text-xl font-semibold">
+          Your details
+        </div>
+        <div>
+          Most of your profile information comes from your Farcaster account. To
+          edit your those details please visit Warpcast.
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="text-foreground font-medium text-sm">
+            Details from Farcaster
+          </div>
+          <div className="border border-border rounded-xl p-10">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <div className="text-foreground font-medium text-sm">Photo</div>
+                <Avatar className="!w-20 !h-20">
+                  <AvatarImage src={session?.user?.image || ""} alt="avatar" />
+                  <AvatarFallback>{session?.user?.name}</AvatarFallback>
+                </Avatar>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="text-foreground font-medium text-sm">Name</div>
+                <Input value={user.name ?? ""} disabled />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="text-foreground font-medium text-sm">
+                  Username
+                </div>
+                <Input
+                  value={user.username ? `@${user.username}` : ""}
+                  disabled
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="text-foreground font-medium text-sm">Bio</div>
+                <Input value={user.bio ?? ""} disabled />
+              </div>
+
+              <ExternalLink href="https://warpcast.com/">
+                <Button className="flex items-center gap-2" variant="secondary">
+                  <div>Edit on Warpcast</div> <MoveUpRight size={12} />
+                </Button>
+              </ExternalLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
