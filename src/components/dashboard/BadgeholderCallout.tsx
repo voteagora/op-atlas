7b4879ddd2d89c37cd83b73cabee15f13b776c2d
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-06-23 12:55:13 -0400

    add badgeholder callout on profile page

---
+++ src/components/dashboard/BadgeholderCallout.tsx
@@ -0,0 +1,33 @@
+import { ArrowRight } from "lucide-react"
+import Image from "next/image"
+
+import { cn } from "@/lib/utils"
+
+import ExternalLink from "../ExternalLink"
+
+export function BadgeholderCallout() {
+  return (
+    <ExternalLink
+      href="https://optimism.deform.cc/season-6-citizen-opt-in/"
+      className={cn(
+        "flex items-center rounded-xl py-8 px-6 w-full",
+        "bg-accent text-accent-foreground",
+      )}
+    >
+      <Image
+        alt="Info"
+        src="/assets/icons/sunny-callout.png"
+        width={48}
+        height={48}
+      />
+      <div className="ml-4 mr-5 flex-1">
+        <p className="font-medium">
+          Calling all badgeholders! Confirm your citizenship for Governance
+          Season 6
+        </p>
+        <div>Apply to vote by July 18.</div>
+      </div>
+      <ArrowRight size={24} />
+    </ExternalLink>
+  )
+}
--- src/lib/badgeholders.ts
+++ src/lib/badgeholders.ts
@@ -1,5 +1,15 @@
+import { Address, checksumAddress } from "viem"
+
+import { UserWithAddresses } from "./types"
+
 // TODO: Replace with final list
-export const BADGEHOLDER_ADDRESSES = [
+export const BADGEHOLDER_ADDRESSES = new Set([
   "0xF0A72469853Db74bEc02301333F29780285b863d", // bwags.eth
   "0xa8DC7990450Cf6A9D40371Ef71B6fa132EeABB0E", // pataguccigirl.eth
-]
+])
+
+export function isBadgeholder(user: UserWithAddresses) {
+  return user.addresses.some((address) =>
+    BADGEHOLDER_ADDRESSES.has(checksumAddress(address.address as Address)),
+  )
+}
