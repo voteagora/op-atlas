9d2154228bcabe83f7d527f83f8bf682fc22e84a
Brian Wagner <brian.wagner35@gmail.com>
2024-06-25 12:37:42 -0400

    Add real badgeholders list

--- src/lib/badgeholders.ts
+++ src/lib/badgeholders.ts
@@ -1,12 +1,11 @@
 import { Address, checksumAddress, getAddress, isAddress } from "viem"
 
+import BadgeholderData from "./badgeholders.json"
 import { UserWithAddresses } from "./types"
 
-// TODO: Replace with final list
-export const BADGEHOLDER_ADDRESSES = new Set([
-  "0xF0A72469853Db74bEc02301333F29780285b863d", // bwags.eth
-  "0xa8DC7990450Cf6A9D40371Ef71B6fa132EeABB0E", // pataguccigirl.eth
-])
+export const BADGEHOLDER_ADDRESSES = new Set(
+  BadgeholderData.map(({ address }) => getAddress(address)),
+)
 
 export function isBadgeholderAddress(address: string) {
   if (!isAddress(address)) {
