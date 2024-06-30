072f3aa3086ac49d1bdea8ad6b14299a8ccffc13
Brian Wagner <bhw2113@columbia.edu>
2024-06-30 14:07:56 -0400

    Wired up more actions for claiming rewards (#148)

--- src/lib/actions/rewards.ts
+++ src/lib/actions/rewards.ts
@@ -1,9 +1,20 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
 import { isAddress } from "viem"
 
-import { getStreams } from "../superfluid"
+import { auth } from "@/auth"
+import {
+  getClaimByAddress,
+  getReward,
+  startClaim,
+  updateClaim,
+} from "@/db/rewards"
 
+import { getStreams } from "../superfluid"
+import { verifyAdminStatus } from "./utils"
+
+// TODO: Can filter by sender once we have it
 export const getActiveStream = async (address: string) => {
   if (!isAddress(address)) {
     return {
@@ -21,5 +32,105 @@ export const getActiveStream = async (address: string) => {
     return {
       error: (error as Error).message,
     }
+  }
+}
+
+export const addAddressToRewardsClaim = async (
+  rewardId: string,
+  address: string,
+) => {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return {
+      error: "Unauthorized",
+    }
+  }
+
+  const reward = await getReward({ id: rewardId })
+  if (!reward) {
+    return {
+      error: "Reward not found",
+    }
+  }
+
+  const isInvalid = await verifyAdminStatus(
+    reward.project.id,
+    session.user.farcasterId,
+  )
+  if (isInvalid?.error) {
+    return isInvalid
+  }
+
+  if (!isAddress(address)) {
+    return {
+      error: "Invalid address",
+    }
+  }
+
+  // Address can't already have a superfluid stream
+  const { streams } = await getStreams({ recipient: address.toLowerCase() })
+  if (streams.length > 0) {
+    return {
+      error: "Address already has a stream",
+    }
+  }
+
+  // Address can't be used by another claim
+  const claim = await getClaimByAddress({ address })
+  if (claim) {
+    return {
+      error: "Address already in use by another project",
+    }
+  }
+
+  // Create the claim
+  await startClaim({ rewardId, address, userId: session.user.id })
+
+  const updated = await getReward({ id: rewardId })
+
+  revalidatePath("/dashboard")
+  revalidatePath("/projects", "layout")
+  revalidatePath("/rewards/[rewardId]/page", "page")
+
+  return {
+    error: null,
+    reward: updated,
+  }
+}
+
+export const completeRewardsClaim = async (rewardId: string) => {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return {
+      error: "Unauthorized",
+    }
+  }
+
+  const reward = await getReward({ id: rewardId })
+  if (!reward) {
+    return {
+      error: "Reward not found",
+    }
+  }
+
+  const isInvalid = await verifyAdminStatus(
+    reward.project.id,
+    session.user.farcasterId,
+  )
+  if (isInvalid?.error) {
+    return isInvalid
+  }
+
+  const claim = await updateClaim({ rewardId, status: "claimed" })
+
+  revalidatePath("/dashboard")
+  revalidatePath("/projects", "layout")
+  revalidatePath("/rewards/[rewardId]/page", "page")
+
+  return {
+    error: null,
+    claim,
   }
 }
