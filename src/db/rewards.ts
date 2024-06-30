67478f1cc7bcce25bec958fa47293423f323fd1b
Brian Wagner <bhw2113@columbia.edu>
2024-06-30 11:23:04 -0400

    Scripts for ingesting reward and kyc data (#141)

--- src/db/rewards.ts
+++ src/db/rewards.ts
@@ -24,3 +24,56 @@ export async function getReward({ id }: { id: string }) {
     },
   })
 }
+
+export async function insertRewards(
+  rewards: {
+    id: string
+    projectId: string
+    amount: number
+  }[],
+) {
+  return prisma.fundingReward.createMany({
+    data: rewards.map((reward) => ({
+      roundId: "4",
+      ...reward,
+    })),
+    skipDuplicates: true,
+  })
+}
+
+export async function startClaim({
+  rewardId,
+  address,
+  userId,
+}: {
+  rewardId: string
+  userId: string
+  address: string
+}) {
+  return prisma.rewardClaim.create({
+    data: {
+      rewardId,
+      address,
+      addressSetById: userId,
+      addressSetAt: new Date(),
+      status: "pending",
+    },
+  })
+}
+
+export async function updateClaim({
+  rewardId,
+  status,
+}: {
+  rewardId: string
+  status: string
+}) {
+  return prisma.rewardClaim.update({
+    where: {
+      rewardId,
+    },
+    data: {
+      status,
+    },
+  })
+}
---
+++ src/scripts/kyc.ts
@@ -0,0 +1,94 @@
+import { readFile } from "fs/promises"
+import { isAddress } from "viem"
+
+import { getReward, updateClaim } from "@/db/rewards"
+
+const CSV_PATH = "kyc.csv"
+
+/**
+ * Expecting a format of:
+ * form_id,project_id,grant_id,l2_address,status
+ *
+ * status can be:
+ * - not started
+ * - cleared
+ */
+
+async function processKYC(entries: string[]) {
+  let counter = 0
+
+  // Skip the header row
+  for (const row of entries.slice(1)) {
+    const fields = row.split(",")
+    if (fields.length !== 5) {
+      if (row.trim() !== "") {
+        console.error("Invalid KYC row:", row)
+      }
+
+      continue
+    }
+
+    counter += 1
+    const [formId, projectId, rewardId, address, status] = fields
+
+    // TODO: Get final status list and see if other action is needed
+    if (status !== "cleared") {
+      console.log(
+        `Skipping reward ${rewardId} (project ${projectId}) - KYC not ready`,
+      )
+      continue
+    }
+
+    const reward = await getReward({ id: rewardId })
+    if (!reward) {
+      console.warn(
+        `Reward ${rewardId} (project ${projectId}) not found, skipping`,
+      )
+      continue
+    }
+    if (!reward.claim) {
+      console.warn(
+        `No claim found for reward ${rewardId} (project ${projectId}), skipping`,
+      )
+      continue
+    }
+
+    // TODO: Do we need to take action on address mismatch?
+    if (
+      !isAddress(address) ||
+      reward.claim.address?.toLowerCase() !== address.toLowerCase()
+    ) {
+      console.warn(
+        `Address mismatch for reward ${rewardId} (address ${address}), skipping`,
+      )
+      continue
+    }
+
+    // Valid, update the status
+    await updateClaim({ rewardId, status: "cleared" })
+    console.log(
+      `Updated KYC status for reward ${rewardId} (project ${projectId})`,
+    )
+  }
+
+  console.log(
+    `Processed KYC details for ${counter} project${counter === 1 ? "" : "s"}`,
+  )
+}
+
+async function updateClaims() {
+  const file = await readFile(CSV_PATH)
+  const entries = file.toString().split("\n")
+
+  return await processKYC(entries)
+}
+
+console.log("Updating claims...")
+
+updateClaims()
+  .then(() => {
+    console.log("Done")
+  })
+  .catch((error) => {
+    console.error("Error updating claims with KYC data:", error)
+  })
