b9a3d30a90d917aad5e5fd10c73c00a37e81e445
Brian Wagner <bhw2113@columbia.edu>
2024-08-27 15:37:01 -0400

    Script for generating a dump of applicants & applications (#121)
    * Script for generating a dump of applicants
    
    * Oops
    
    * fix yarn lock
    
    * :hammer: add applications export script
    
    ---------
    
    Co-authored-by: stepandel <stepandel@gmail.com>

---
+++ src/scripts/applications.ts
@@ -0,0 +1,111 @@
+import { PrismaClient } from "@prisma/client"
+import { writeFile } from "fs/promises"
+
+const prisma = new PrismaClient()
+
+async function getApplications() {
+  const applications = await prisma.application.findMany({
+    where: {
+      roundId: "5",
+    },
+    distinct: ["projectId"],
+    select: {
+      attestationId: true,
+      projectDescriptionOptions: true,
+      impactStatementAnswer: {
+        include: {
+          impactStatement: {
+            include: {
+              category: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      category: {
+        select: {
+          name: true,
+          description: true,
+        },
+      },
+      project: {
+        include: {
+          team: {
+            include: {
+              user: true,
+            },
+          },
+          organization: {
+            include: {
+              organization: true,
+            },
+          },
+          repos: true,
+          contracts: true,
+          funding: true,
+          rewards: true,
+          links: true,
+        },
+      },
+    },
+  })
+
+  return applications
+}
+
+const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY
+if (!NEYNAR_API_KEY) {
+  throw new Error("NEYNAR_API_KEY is missing from env")
+}
+
+type FarcasterUser = {
+  fid: number
+  username: string
+  display_name: string
+  custody_address: string
+  verified_addresses: {
+    eth_addresses: string[]
+    sol_addresses: string[]
+  }
+}
+
+async function getFarcasterProfiles(farcasterIds: string[]) {
+  const params = new URLSearchParams({ fids: farcasterIds.join(",") })
+  const url = `https://api.neynar.com/v2/farcaster/user/bulk?${params.toString()}`
+  const options = {
+    headers: { accept: "application/json", api_key: NEYNAR_API_KEY ?? "" },
+  }
+
+  const results = await fetch(url, options)
+  const data = (await results.json()) as { users: FarcasterUser[] }
+  return data.users
+}
+
+async function generateApplicationData() {
+  const applications = await Promise.all(
+    (
+      await getApplications()
+    ).map(async (application) => {
+      // get farcasterId for each team member
+      await Promise.all(
+        application.project.team.map(async (member) => {
+          const user = await getFarcasterProfiles([member.user.farcasterId])
+          member.user = {
+            ...member.user,
+            ...user[0],
+          }
+        }),
+      )
+
+      return application
+    }),
+  )
+
+  // write to file
+  await writeFile("applications.json", JSON.stringify(applications, null, 2))
+}
+
+generateApplicationData()
