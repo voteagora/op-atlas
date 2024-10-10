76c5a9b5533bbd9e40e37ea0aede47a2bcdbeb49
Michael Gingras <mcg79@cornell.edu>
2024-10-10 13:05:32 -0600

    Adds better visibility into unsaved applications
    * Adds better details around unsaved changes
    
    * Adds toast to all steps of project application if the user has unsubmitted changes
    
    * Adds icons to card if published onchain
    
    * Readds strict mode

--- src/components/projects/details/ProjectDetailsForm.tsx
+++ src/components/projects/details/ProjectDetailsForm.tsx
@@ -5,8 +5,8 @@ import { Organization, Project } from "@prisma/client"
 import { Plus } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { useRouter, useSearchParams } from "next/navigation"
-import { useEffect, useMemo, useState } from "react"
+import { useRouter } from "next/navigation"
+import { useMemo, useState } from "react"
 import { useFieldArray, useForm } from "react-hook-form"
 import { toast } from "sonner"
 import { z } from "zod"
@@ -261,9 +261,7 @@ export default function ProjectDetailsForm({
       })
 
       toast.promise(promise, {
-        loading: isCreating
-          ? "Creating project onchain..."
-          : "Saving project onchain...",
+        loading: isCreating ? "Creating project onchain..." : "Saving project",
         success: (project) => {
           isSave
             ? router.replace(`/projects/${project.id}/details`)
