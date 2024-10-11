45ee61a2469d82312064e9b29d171f556a84a3c2
stepandel <stepandel@gmail.com>
2024-10-11 11:00:38 -0700

    Revert "temp remove opengraph image"
    This reverts commit a81e4082070887cb0b17670ba52f6b57ee8a8740.

---
+++ src/app/rewards/[rewardId]/opengraph-image.tsx
@@ -0,0 +1,111 @@
+import { ImageResponse } from "next/og"
+
+import { getReward } from "@/db/rewards"
+
+export const size = {
+  width: 1600,
+  height: 900,
+}
+
+export const contentType = "image/png"
+
+export default async function OGImage({
+  params,
+}: {
+  params: { rewardId: string }
+}) {
+  const claim = await getReward({ id: params.rewardId })
+  const projectName = claim?.project.name
+  const reward = claim?.amount || 0
+  const thumbnailUrl = claim?.project.thumbnailUrl
+
+  const soraRegular = await fetch(
+    new URL("../../assets/Sora-Regular.otf", import.meta.url),
+  ).then((res) => res.arrayBuffer())
+
+  const soraSemiBold = await fetch(
+    new URL("../../assets/Sora-SemiBold.otf", import.meta.url),
+  ).then((res) => res.arrayBuffer())
+
+  const soraBold = await fetch(
+    new URL("../../assets/Sora-Bold.otf", import.meta.url),
+  ).then((res) => res.arrayBuffer())
+
+  const bgImage = await fetch(
+    new URL("../../assets/round-5-reward.png", import.meta.url),
+  ).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          fontFamily: '"Sora"',
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+        }}
+      >
+        <img
+          // @ts-ignore
+          src={bgImage}
+          style={{ position: "absolute" }}
+          tw="absolute"
+          alt="optimism red background"
+        />
+
+        <div tw="flex flex-row justify-between px-[55px] mt-[340px]">
+          <img
+            src={thumbnailUrl!}
+            tw="h-[350px] w-[350px] mr-[40px] rounded-[35px]"
+            alt="project thumbnail"
+          />
+          <div tw="flex flex-col self-end">
+            <h3 tw="text-5xl m-0 text-[#FE1138]">{projectName} received</h3>
+            <h4
+              tw="text-[120px] m-0"
+              style={{
+                backgroundImage:
+                  "linear-gradient(90deg, #FE1138 0%, #FE4FE2 100%)",
+                backgroundClip: "text",
+                // @ts-ignore TS doesn't know about webkit prefixes
+                "-webkit-background-clip": "text",
+                color: "transparent",
+              }}
+            >
+              {reward.toString()} OP
+            </h4>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: size.width,
+      height: size.height,
+      fonts: [
+        {
+          name: "Sora",
+          data: soraRegular,
+          weight: 400,
+          style: "normal",
+        },
+        {
+          name: "Sora",
+          data: soraSemiBold,
+          weight: 600,
+          style: "normal",
+        },
+        {
+          name: "Sora",
+          data: soraBold,
+          weight: 700,
+          style: "normal",
+        },
+      ],
+    },
+  )
+}
