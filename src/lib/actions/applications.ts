7ce530d61361438d355ffd908973c6c0e3b0648c
stepandel <stepandel@gmail.com>
2024-10-17 13:14:36 -0700

    clear up whitelist

--- src/lib/actions/applications.ts
+++ src/lib/actions/applications.ts
@@ -10,19 +10,12 @@ import { getUserById } from "@/db/users"
 
 import { createApplicationAttestation } from "../eas"
 import { uploadToPinata } from "../pinata"
-import { ApplicationWithDetails, CategoryWithImpact } from "../types"
+import { CategoryWithImpact } from "../types"
 import { APPLICATIONS_CLOSED, getProjectStatus } from "../utils"
 import { formatApplicationMetadata } from "../utils/metadata"
 import { verifyAdminStatus } from "./utils"
 
-const whitelist = [
-  "0x61400c6b679bc467d522c7124819332c8a660716c55ea71f76f708d6bc296c22",
-  "0x008875f970469e090a5a843c68e3f8444e110a741990f03938e4ea42df8d11a2",
-  "0xdff778291bf6893ac1c67540cb7b552781721138767dbf59dd1d1ba132a4c377",
-  "0x09b585a065b43e85d9d86c9901b13fa80ac872228c57ffb23ed0063d1f2da28a",
-  "0x850df51e29f2846a5f085d88e6b6fc13fad51ad7161f8a825ed09d470668161a",
-  "0x8d7569742539aab697bd8825e7c49883743778b38b6090660fbdf84ec2c3938f",
-]
+const whitelist: string[] = []
 
 interface SubmitApplicationRequest {
   projectId: string
