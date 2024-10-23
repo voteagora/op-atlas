a8590bc7c6eefdde623681a046faba5dcb3d9757
stepandel <stepandel@gmail.com>
2024-10-23 14:29:01 -0700

    update reward load script for R5 rewards

--- src/scripts/rewards.ts
+++ src/scripts/rewards.ts
@@ -1,8 +1,7 @@
-import { readFile } from "fs/promises"
+import { readFile, writeFile } from "fs/promises"
 import { splitEvery } from "ramda"
 
-import { insertRewards } from "@/db/rewards"
-import { nanoid } from "@/lib/utils"
+import { prisma } from "@/db/client"
 
 const CSV_PATH = "rewards.csv"
 
@@ -17,10 +16,18 @@ async function ingestRewards() {
   const file = await readFile(CSV_PATH)
   const entries = file.toString().split("\n")
 
-  const rewards: { id: string; projectId: string; amount: number }[] = []
+  let grantId = 208
+
+  const rewards: {
+    id: string
+    projectId: string
+    amount: number
+    applicationId: string
+    name: string
+  }[] = []
   for (const row of entries) {
     const fields = row.split(",")
-    if (fields.length !== 4) {
+    if (fields.length !== 3) {
       if (row.trim() !== "") {
         console.error("Invalid reward row:", row)
       }
@@ -28,22 +35,63 @@ async function ingestRewards() {
       continue
     }
 
-    const [projectId, grantId, name, amount] = fields
+    const [applicationId, name, amount] = fields
+
+    console.log({
+      applicationId,
+      name,
+      amount,
+    })
+
+    const project = await prisma.application.findFirst({
+      where: {
+        attestationId: applicationId,
+      },
+    })
+
+    if (!project) {
+      throw new Error(`Project not found: ${applicationId}`)
+    }
 
     rewards.push({
-      id: grantId,
-      projectId,
+      id: grantId.toString(),
+      projectId: project.projectId,
       amount: parseFloat(amount),
+      applicationId,
+      name,
     })
+
+    grantId++
   }
 
   for (const batch of splitEvery(50, rewards)) {
-    await insertRewards(batch)
+    console.log("Inserting rewards...")
+    console.log(batch)
+
+    await prisma.fundingReward.createMany({
+      data: batch.map((reward) => ({
+        roundId: "5",
+        id: reward.id,
+        projectId: reward.projectId,
+        amount: reward.amount,
+      })),
+      skipDuplicates: true,
+    })
   }
 
   console.log(
     `Inserted ${rewards.length} reward${rewards.length === 1 ? "" : "s"}`,
   )
+
+  // write the rewards to csv file (projectId, rewardId)
+  const csv = rewards
+    .map(
+      ({ id, projectId, applicationId, name, amount }) =>
+        `${id},${projectId},${applicationId},${name},${amount}`,
+    )
+    .join("\n")
+
+  await writeFile("rewards-output.csv", csv)
 }
 
 console.log("Ingesting rewards...")
