fedd16473d95351c8dd6932a0a6888bfe86ac010
stepandel <stepandel@gmail.com>
2024-12-09 18:00:09 -0800

    use correct schema id for citizens

--- schemas.config.ts
+++ schemas.config.ts
@@ -1,6 +1,6 @@
 const schemas = {
   citizen: {
-    id: "0x5ebff8ad62d203585850493a9699d7f32d0de739ff7f7421f1ad64d6ddf7749d" as `0x${string}`,
+    id: "0xc35634c4ca8a54dce0a2af61a9a9a5a3067398cb3916b133238c4f6ba721bc8a" as `0x${string}`,
     name: "citizen",
   },
 };
