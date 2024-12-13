8c11bc3ff7dbc750fcee3be7c1a42d21456724dc
Stepan Arsentjev <stepandel@gmail.com>
2024-12-13 14:13:38 -0800

    Monorepo (#387)
    * refactor to monorepo
    
    * add missing farcaster auth dependency
    
    * move funding.json to root
    
    * install missing ethers package
    
    * remove typescript from dev dependencies?
    
    * commit package.json
    
    * fix build
    
    * update READMEs
    
    * remove redundant text
    
    * gitignore DS_Store
    
    * remove DS_Store

--- app/src/lib/utils/og.ts
+++ app/src/lib/utils/og.ts
@@ -1,7 +1,7 @@
 import fs from "fs/promises"
 import path from "path"
 
-export async function loadFont(filename: string): Promise<ArrayBuffer> {
+export async function loadFont(filename: string): Promise<Buffer> {
   const filePath = path.join(process.cwd(), "public", "fonts", filename)
   return fs.readFile(filePath)
 }
