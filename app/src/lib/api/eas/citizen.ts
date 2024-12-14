0dacbbae9c0dca7130c9ed02b784059fa1de64bc
stepandel <stepandel@gmail.com>
2024-12-14 14:20:17 -0800

    import eas-indexer types to app

--- app/src/lib/api/eas/citizen.ts
+++ app/src/lib/api/eas/citizen.ts
@@ -1,10 +1,12 @@
+import { Citizen } from "eas-indexer/src/types"
+
 import { createFetchClient } from "../common"
 
 const BASE_URL = "atlas-eas-indexer-production.up.railway.app"
 
 const client = createFetchClient(BASE_URL)
 
-export const getCitizen = async (address: string) => {
+export const getCitizen = async (address: string): Promise<Citizen | null> => {
   try {
     const response = await client(`/citizen/${address}`)
     return response
