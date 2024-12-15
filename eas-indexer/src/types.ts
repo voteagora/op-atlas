ac5eceedc323e7ebc72d6e239b56b18ae26e75b5
stepandel <stepandel@gmail.com>
2024-12-14 16:34:55 -0800

    make a general attestations endpoint

--- eas-indexer/src/types.ts
+++ eas-indexer/src/types.ts
@@ -4,3 +4,10 @@ export type Citizen = typeof dbSchema.citizen.$inferSelect;
 export type Badgeholder = typeof dbSchema.badgeholder.$inferSelect;
 export type GovContribution = typeof dbSchema.gov_contribution.$inferSelect;
 export type RfVoter = typeof dbSchema.rf_voter.$inferSelect;
+
+export type Attestation = {
+  id: string;
+  entity: string;
+  address: string;
+  subtext: string;
+};
