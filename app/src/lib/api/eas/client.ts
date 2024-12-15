13f6b6a2f3a31f2f35d69be1bc188caf335a8533
stepandel <stepandel@gmail.com>
2024-12-15 15:06:48 -0800

    get indexer base url from env vars

--- app/src/lib/api/eas/client.ts
+++ app/src/lib/api/eas/client.ts
@@ -1,5 +1,5 @@
 import { createFetchClient } from "../common"
 
-const BASE_URL = "atlas-eas-indexer-production.up.railway.app"
+const BASE_URL = process.env.EAS_INDEXER_API_URL!
 
 export const easClient = createFetchClient(BASE_URL)
