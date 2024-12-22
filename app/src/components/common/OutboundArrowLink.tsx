7241e11dec81913a87a14ac2a2e35bb8aabb5640
Jacob Homanics <homanicsjake@gmail.com>
2024-12-22 17:43:43 -0600

    upsized and removed hover on arrow. fixed gap

--- app/src/components/common/OutboundArrowLink.tsx
+++ app/src/components/common/OutboundArrowLink.tsx
@@ -20,16 +20,15 @@ function OutboundArrowLink({
       href={target}
       target="_blank"
       rel="noopener noreferrer"
-      className={cn("group flex items-center gap-x-3", className)}
+      className={cn("group flex items-center gap-x-1", className)}
     >
       {icon}
       <span className="group-hover:underline">{text}</span>
       <Image
         src="/assets/icons/arrow-up-right.svg"
-        width={10}
-        height={10}
+        width={16}
+        height={16}
         alt="External link"
-        className="ml-1 opacity-0 group-hover:opacity-100 transition-opacity"
       />
       {subtext && <span className="text-md text-gray-500">{subtext}</span>}
     </a>
