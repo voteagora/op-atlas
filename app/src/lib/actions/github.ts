c40c5f9172204af32a5235f3e056c304e92a4159
Jacob Homanics <homanicsjake@gmail.com>
2024-12-23 11:18:22 -0600

    Update github.ts

--- app/src/lib/actions/github.ts
+++ app/src/lib/actions/github.ts
@@ -1,18 +1,6 @@
 import { getUser as getGithubUser } from "../github"
 
 export const getUser = async (username: string) => {
-  try {
-    const user = await getGithubUser(username)
-    if (user) {
-      return {
-        error: null,
-        user: user.data ?? null,
-      }
-    }
-  } catch (error: unknown) {
-    console.error("Error searching for user", (error as Error).message)
-    return {
-      error: "Error searching for user",
-    }
-  }
+  const user = await getGithubUser(username)
+  return user
 }
