031c971ff10a4aeb343eb08bd451369af9f61c99
Jacob Homanics <homanicsjake@gmail.com>
2024-12-22 19:28:32 -0600

    fleshed out 404 page

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -1,11 +1,45 @@
+import OutboundArrowLink from "@/components/common/OutboundArrowLink"
+import Image from "next/image"
 import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center gap-6 mt-64">
+      <Image
+        src={"/assets/icons/sunny-stars-custom-gimp-edit.png"}
+        alt="Sunny Logo"
+        width={160}
+        height={160}
+      />
+      <h4 className="text-[#0F111A] font-semibold">
+        The page you're looking for doesn't exist.
+      </h4>
+
+      <div className="flex gap-3">
+        <OutboundArrowLink
+          className="bg-[#F2F3F8] pt-[10px] pb-[10px] pl-4 pr-4 rounded-lg font-medium"
+          text={"Optimism"}
+          target={"https://optimism.io"}
+        />
+
+        <OutboundArrowLink
+          className="bg-[#F2F3F8] pt-[10px] pb-[10px] pl-4 pr-4 rounded-lg font-medium"
+          text={"Retro Funding"}
+          target={"/"}
+        />
+
+        <OutboundArrowLink
+          className="bg-[#F2F3F8] pt-[10px] pb-[10px] pl-4 pr-4 rounded-lg font-medium"
+          text={"Forum"}
+          target={"https://gov.optimism.io"}
+        />
+
+        <OutboundArrowLink
+          className="bg-[#F2F3F8] pt-[10px] pb-[10px] pl-4 pr-4 rounded-lg font-medium"
+          text={"Delegates"}
+          target={"https://vote.optimism.io/delegates"}
+        />
+      </div>
     </div>
   )
 }
