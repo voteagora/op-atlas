bf3dc8bb1a03bb8fe12323db419d2a1a64aba98d
stepandel <stepandel@gmail.com>
2025-01-02 11:30:27 -0800

    update pinata config to unclude NEXT_PUBLIC_GATEWAY_URL

--- app/src/lib/pinata.ts
+++ app/src/lib/pinata.ts
@@ -1,12 +1,16 @@
 import { PinataSDK } from "pinata-web3"
 
 if (!process.env.PINATA_JWT) {
-  throw new Error("PINATA_JWT")
+  throw new Error("PINATA_JWT is missing from env")
+}
+
+if (!process.env.NEXT_PUBLIC_GATEWAY_URL) {
+  throw new Error("NEXT_PUBLIC_GATEWAY_URL is missing from env")
 }
 
 const pinata = new PinataSDK({
   pinataJwt: process.env.PINATA_JWT,
-  pinataGateway: "olive-capitalist-mule-825.mypinata.cloud",
+  pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL,
 })
 
 export async function testConnection() {
