ca832b7f3ba0754ee5c033f58f30b0373db995c9
stepandel <stepandel@gmail.com>
2025-01-02 09:52:06 -0800

    link spacing
    Co-authored-by: Jacob Homanics <homanicsjake@gmail.com>

--- app/src/components/common/OutboundArrowLink.tsx
+++ app/src/components/common/OutboundArrowLink.tsx
@@ -23,14 +23,16 @@ function OutboundArrowLink({
       className={cn("group flex items-center gap-x-3", className)}
     >
       {icon}
-      <span className="group-hover:underline">{text}</span>
-      <Image
-        src="/assets/icons/arrow-up-right.svg"
-        width={10}
-        height={10}
-        alt="External link"
-        className="ml-1 opacity-0 group-hover:opacity-100 transition-opacity"
-      />
+      <span className="group-hover:underline inline-flex items-center">
+        {text}
+        <Image
+          src="/assets/icons/arrow-up-right.svg"
+          width={10}
+          height={10}
+          alt="External link"
+          className="opacity-0 group-hover:opacity-100 transition-opacity ml-0.5"
+        />
+      </span>
       {subtext && <span className="text-md text-gray-500">{subtext}</span>}
     </a>
   )
