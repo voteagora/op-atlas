02e4e85352ba937e7f0b5fe2381516787dc7b7c2
Nikola Gigić <nik.gigic@gmail.com>
2025-01-13 23:55:45 +0100

    fix(issue-393): reset form with new data on save (#470)

--- app/src/components/projects/repos/ReposForm.tsx
+++ app/src/components/projects/repos/ReposForm.tsx
@@ -225,6 +225,7 @@ export const ReposForm = ({ project }: { project: ProjectWithDetails }) => {
 
         !isSave && router.push(`/projects/${project.id}/contracts`)
         setIsSaving(false)
+        form.reset({ links, githubRepos: projectRepos })
         toast.success("Project saved")
       } catch (error) {
         toast.error("There was an error updating your Repos and Links")
@@ -232,6 +233,7 @@ export const ReposForm = ({ project }: { project: ProjectWithDetails }) => {
         console.error("Error saving packages", error)
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [project.id, router],
   )
 
