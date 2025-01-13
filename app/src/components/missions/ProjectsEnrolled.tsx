eebac338d374c4b55c3fa227dc86d1af4bed8480
Jacob Homanics <homanicsjake@gmail.com>
2025-01-12 19:29:36 -0600

    projects enrolled section

--- app/src/components/missions/ProjectsEnrolled.tsx
+++ app/src/components/missions/ProjectsEnrolled.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 export const ProjectsEnrolled = ({
   className,
@@ -15,12 +16,72 @@ export const ProjectsEnrolled = ({
   opRewarded: string
   avgOpRewardPerProject: string
 }) => {
+  const projects = [
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+    {
+      icon: "/assets/icons/uniswap.png",
+    },
+  ]
+
   return (
     <div className={cn("flex flex-col gap-y-6", className)}>
       {projectsEnrolled > 0 && (
         <div className="flex flex-col gap-y-3 p-6 border border-2 border-grey-900 rounded-xl">
           <p className="font-bold">{projectsEnrolled} projects enrolled</p>
-          <div className="w-full bg-black rounded-lg h-[126px]"></div>
+
+          <div>
+            <div
+              className={`flex flex-wrap gap-1 max-h-[120px] rounded-lg overflow-hidden items-start`}
+            >
+              {projects.map((project: any, index: number) => {
+                return (
+                  <Image
+                    src={project.icon}
+                    width={44}
+                    height={44}
+                    alt="Project"
+                  />
+                )
+              })}
+            </div>
+            {projects.length > 10 ? (
+              <div className="w-full bg-gray-300 h-[2px]" />
+            ) : (
+              <></>
+            )}
+          </div>
 
           <LittleSection
             title={`${units} Units`}
