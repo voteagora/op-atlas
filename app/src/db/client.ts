c34f51b97b58216cad4c9aed9305c6b0f969d9e6
stepandel <stepandel@gmail.com>
2025-01-14 15:37:21 -0800

    add query logging

--- app/src/db/client.ts
+++ app/src/db/client.ts
@@ -1,7 +1,48 @@
 import { PrismaClient } from "@prisma/client"
 
+import { timeThis } from "@/logging"
+
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return makePrismaClient() as PrismaClient
+}
+
+// Logging middleware
+const makePrismaClient = () => {
+  const execRaw = async (
+    query: (args: any) => Promise<any>,
+    args: any,
+    operation: string,
+  ) => {
+    return await timeThis(async () => await query(args), {
+      operation,
+      args,
+    })
+  }
+  return new PrismaClient().$extends({
+    query: {
+      $allModels: {
+        async $allOperations({ operation, model, args, query }) {
+          return await timeThis(async () => await query(args), {
+            model,
+            operation,
+            args,
+          })
+        },
+      },
+      async $queryRaw({ args, query, operation }) {
+        return await execRaw(query, args, operation)
+      },
+      async $executeRaw({ args, query, operation }) {
+        return await execRaw(query, args, operation)
+      },
+      async $queryRawUnsafe({ args, query, operation }) {
+        return await execRaw(query, args, operation)
+      },
+      async $executeRawUnsafe({ args, query, operation }) {
+        return await execRaw(query, args, operation)
+      },
+    },
+  })
 }
 
 declare const globalThis: {
