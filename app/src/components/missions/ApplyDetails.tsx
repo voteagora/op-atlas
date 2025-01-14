e9add9c664ae6fb16560a93b820443a4c14cbb2d
Jacob Homanics <homanicsjake@gmail.com>
2025-01-13 21:09:17 -0600

    apply details - no projects

--- app/src/components/missions/ApplyDetails.tsx
+++ app/src/components/missions/ApplyDetails.tsx
@@ -4,6 +4,8 @@ import { FundingRound } from "@/lib/mocks"
 import { Project } from "@/components/missions/Project"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { ProjectWithDetails } from "@/lib/types"
+import { Button } from "../ui/button"
+import { useRouter } from "next/navigation"
 
 export function ApplyDetails({
   projects,
@@ -13,6 +15,8 @@ export function ApplyDetails({
   round: FundingRound
 }) {
   const [currentTab, setCurrentTab] = useState("details")
+
+  const router = useRouter()
 
   return (
     <div className="mt-16 bg-background flex flex-col w-full max-w-5xl rounded-3xl z-10">
@@ -35,14 +39,47 @@ export function ApplyDetails({
           {/* application details content */}
           <TabsContent value="details">
             <p className="text-2xl font-bold">Choose projects</p>
-            {projects.map((field, index) => (
-              <Project
-                key={field.id}
-                index={index}
-                project={field}
-                round={round}
-              />
-            ))}{" "}
+
+            {projects.length > 0 ? (
+              projects.map((field, index) => (
+                <Project
+                  key={field.id}
+                  index={index}
+                  project={field}
+                  round={round}
+                />
+              ))
+            ) : (
+              <div className="flex flex-col items-center justify-center gap-y-5 p-10 border border-2 border-grey-900 rounded-xl">
+                <p className="font-bold">
+                  {"You haven't added or joined any projects"}
+                </p>
+
+                <p className="text-sm text-secondary-foreground text-center">
+                  {
+                    "To apply for this Retro Funding Mission, first add your project to OP Atlas."
+                  }
+                </p>
+
+                <div className="flex gap-4">
+                  <Button
+                    className="bg-optimismRed text-white w-44"
+                    variant={"outline"}
+                  >
+                    Add Project
+                  </Button>
+                  <Button
+                    className="w-44"
+                    variant={"outline"}
+                    onClick={() => {
+                      router.push(`/missions/${round.pageName}`)
+                    }}
+                  >
+                    View Mission Details
+                  </Button>
+                </div>
+              </div>
+            )}
           </TabsContent>
         </div>
       </Tabs>
