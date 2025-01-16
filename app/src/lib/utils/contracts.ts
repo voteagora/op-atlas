da4b6b33f91f0c91e9e97bd08df8d935bb9b0857
Jacob Homanics <homanicsjake@gmail.com>
2025-01-16 16:15:33 -0600

    added support for multiple new chains

--- app/src/lib/utils/contracts.ts
+++ app/src/lib/utils/contracts.ts
@@ -1,10 +1,50 @@
-import { base, fraxtal, mode, optimism, zora } from "viem/chains"
+import {
+  base,
+  fraxtal,
+  mode,
+  optimism,
+  zora,
+  worldchain,
+  lisk,
+  metalL2,
+  superseed,
+  shape,
+  swan,
+  inkSepolia,
+} from "viem/chains"
+import { defineChain } from "viem"
+
+export const ink = defineChain({
+  id: 57073,
+  name: "Ink",
+  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc-gel.inkonchain.com"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Blockscout",
+      url: "https://explorer.inkonchain.com/",
+      apiUrl: "https://explorer.inkonchain.com/api/v2",
+    },
+  },
+  sourceId: 1,
+})
 
 export enum Chain {
   Base = base.id,
   Fraxtal = fraxtal.id,
+  Ink = ink.id,
+  Lisk = lisk.id,
+  MetalL2 = metalL2.id,
   Mode = mode.id,
   Optimism = optimism.id,
+  Shape = shape.id,
+  Superseed = superseed.id,
+  Swan = swan.id,
+  Worldchain = worldchain.id,
   Zora = zora.id,
 }
 
