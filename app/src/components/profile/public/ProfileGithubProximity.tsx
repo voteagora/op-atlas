6c18eb2fb0c7251c7884dc998393afcfe54c1e4e
Nikola Gigic <nik.gigic@gmail.com>
2025-01-18 02:08:38 +0100

    feat: public page stack proximity

--- app/src/components/profile/public/ProfileGithubProximity.tsx
+++ app/src/components/profile/public/ProfileGithubProximity.tsx
@@ -2,9 +2,9 @@ import { ChevronDown, ChevronUp } from "lucide-react"
 import { useState } from "react"
 
 import OutboundArrowLink from "@/components/common/OutboundArrowLink"
+import { Badge } from "@/components/ui/badge"
 import useGithubProximity from "@/hooks/useGithubProximity"
 import { UserWithAddresses } from "@/lib/types"
-import { Badge } from "@/components/ui/badge"
 
 const labels = [
   {
@@ -37,7 +37,7 @@ function ProfileGithubProximity({ user }: { user: UserWithAddresses }) {
   if (data.percentile < 0.8) return null
 
   return (
-    <div className="flex flex-col gap-y-4 mt-12">
+    <div className="flex flex-col space-y-4">
       <h2 className="text-xl font-medium flex items-center gap-x-2">
         OP Stack Proximity
         <span className="text-xs text-secondary-foreground bg-gray-100 dark:bg-gray-800 rounded-full px-2 py-1">
@@ -45,75 +45,77 @@ function ProfileGithubProximity({ user }: { user: UserWithAddresses }) {
         </span>
       </h2>
 
-      <div className="grid grid-cols-5 gap-x-8 bg-background rounded-xl border border-gray-200 p-8">
-        <div className="col-span-3 font-semibold">
-          <div>
-            {user.name} ranks in the top{" "}
-            {Math.max(Math.round((1 - data.percentile) * 100), 1)}% of
-            developers on Github for proximity to the OP Stack
-          </div>
-          <p className="mt-2 text-secondary-foreground">
-            <OutboundArrowLink
-              text={`@${user.github}`}
-              target={`https://github.com/${user.github}`}
-              className="text-sm text-gray-500"
-            />
-          </p>
-        </div>
-
-        <div className="col-span-2 flex items-start">
-          <div className="relative w-48 h-32">
-            <svg className="w-[192px] h-[128px]">
-              <defs>
-                <linearGradient
-                  id="redGradient"
-                  x1="0%"
-                  y1="0%"
-                  x2="100%"
-                  y2="0%"
-                >
-                  <stop offset="0%" style={{ stopColor: "#fee2e2" }} />
-                  <stop offset="100%" style={{ stopColor: "#ef4444" }} />
-                </linearGradient>
-              </defs>
-              <path
-                d="M 12 96 A 84 84 0 0 1 180 96"
-                stroke="#e5e7eb"
-                strokeWidth="8"
-                fill="none"
-              />
-              <path
-                d="M 12 96 A 84 84 0 0 1 180 96"
-                stroke="url(#redGradient)"
-                strokeWidth="8"
-                fill="none"
-                strokeDasharray={`${Math.PI * 84}`}
-                strokeDashoffset={`${Math.PI * 84 * (1 - data.percentile)}`}
-                className="transition-all duration-1000 ease-out"
-              />
-            </svg>
-            <div className="absolute inset-0 flex flex-col items-center justify-center mt-5">
-              <span className="text-4xl font-extrabold">
-                {Math.round(data.percentile * 100)}
-                <span className="text-lg font-normal">th</span>
-              </span>
-              <span className="text-sm text-gray-500">PERCENTILE</span>
+      <div className="bg-background rounded-xl border border-gray-200 p-6">
+        <div className="flex lg:flex-row flex-col-reverse">
+          <div className="font-semibold">
+            <div>
+              {user.name} ranks in the top{" "}
+              {Math.max(Math.round((1 - data.percentile) * 100), 1)}% of
+              developers on Github for proximity to the OP Stack
             </div>
+            <p className="mt-2 text-secondary-foreground">
+              <OutboundArrowLink
+                text={`@${user.github}`}
+                target={`https://github.com/${user.github}`}
+                className="text-sm text-gray-500"
+              />
+            </p>
           </div>
-          {label && (
-            <Badge
-              variant="destructive"
-              className="text-xs font-medium px-2 py-1 rounded-full text-white gap-x-1"
-            >
-              <span className="text-white">{label.emoji}</span>{" "}
-              <span className="text-white font-normal whitespace-nowrap">
-                {label.text}
-              </span>
-            </Badge>
-          )}
+          <div className="flex items-start">
+            <div className="relative w-48 h-32">
+              <svg className="w-48 h-32">
+                <defs>
+                  <linearGradient
+                    id="redGradient"
+                    x1="0%"
+                    y1="0%"
+                    x2="100%"
+                    y2="0%"
+                  >
+                    <stop offset="0%" style={{ stopColor: "#fee2e2" }} />
+                    <stop offset="100%" style={{ stopColor: "#ef4444" }} />
+                  </linearGradient>
+                </defs>
+                <path
+                  d="M 12 96 A 84 84 0 0 1 180 96"
+                  stroke="#e5e7eb"
+                  strokeWidth="8"
+                  fill="none"
+                />
+                <path
+                  d="M 12 96 A 84 84 0 0 1 180 96"
+                  stroke="url(#redGradient)"
+                  strokeWidth="8"
+                  fill="none"
+                  strokeDasharray={`${Math.PI * 84}`}
+                  strokeDashoffset={`${Math.PI * 84 * (1 - data.percentile)}`}
+                  className="transition-all duration-1000 ease-out"
+                />
+              </svg>
+              <div className="absolute inset-0 flex flex-col items-center justify-center mt-5">
+                <span className="text-4xl font-extrabold">
+                  {Math.round(data.percentile * 100)}
+                  <span className="text-lg font-normal">th</span>
+                </span>
+                <span className="text-sm text-gray-500">PERCENTILE</span>
+              </div>
+            </div>
+            {label && (
+              <div className="w-full flex items-center justify-center">
+                <Badge
+                  variant="destructive"
+                  className="text-xs font-medium px-2 py-1 rounded-full text-white space-x-1 w-fit"
+                >
+                  <span className="text-white">{label.emoji}</span>{" "}
+                  <span className="text-white font-normal whitespace-nowrap">
+                    {label.text}
+                  </span>
+                </Badge>
+              </div>
+            )}
+          </div>
         </div>
-
-        <div className="col-span-5 ">
+        <div>
           <div className="flex flex-col">
             <div
               className={`content mt-4 text-sm text-gray-600 space-y-4 ${
