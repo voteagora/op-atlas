c46faca287477d336d706e6a40c1cfe631c73bb1
Jacob Homanics <homanicsjake@gmail.com>
2025-01-24 10:46:31 -0600

    Update Header.tsx

--- app/src/components/missions/details/Header.tsx
+++ app/src/components/missions/details/Header.tsx
@@ -8,26 +8,13 @@ import { useMissionFromPath } from "@/hooks/db/useMissionFromPath"
 
 import { NewIn2025Callout } from "../common/callouts/NewIn2025Callout"
 export default function Header() {
-  //{
-  //   name,
-  //   description,
-  //   iconUrl,
-  //   startsAt,
-  //   endsAt,
-  // }: {
-  //   name: string
-  //   description: string
-  //   iconUrl?: string
-  //   startsAt: Date | undefined
-  //   endsAt: Date | undefined
-  // }
   const mission = useMissionFromPath()
 
   return (
     <>
       <h2 className="text-4xl mb-2">{mission?.name}</h2>
       <div className="flex gap-2 mb-6 items-center">
-        <p className="font-light text-gray-700">
+        <p className="text-gray-700">
           <span>{format(mission!.startsAt, "MMM d")}</span>
           <span>{" - "}</span>
           {<span>{format(mission!.endsAt, "MMM d, yyyy")}</span>}
@@ -41,7 +28,7 @@ export default function Header() {
           className="h-3 w-3"
         />
 
-        <p className="font-light text-gray-700">Open for applications</p>
+        <p className="text-gray-700">Open for applications</p>
       </div>
       {mission?.iconUrl && (
         <Image
@@ -54,20 +41,20 @@ export default function Header() {
       )}
 
       <div className="mb-5">
-        <span className="font-semibold ">{`Retro Funding: ${mission?.name}`}</span>{" "}
+        <span className="">{`Retro Funding: ${mission?.name}`}</span>{" "}
         <span className="">{mission?.details}</span>
       </div>
 
       <ul className="list-disc pl-6">
         <li>
-          <span className="font-bold ">{"Timeline:"}</span>
+          <span className="">{"Timeline:"}</span>
           <span className="">{` The program will take place from `}</span>
           <span>{format(mission!.startsAt, "MMM d")}</span>
           <span>{" - "}</span>
           {<span>{format(mission!.endsAt, "MMM d, yyyy")}</span>}.
         </li>
         <li>
-          <span className="font-semibold ">{"Application periods:"}</span>
+          <span className="">{"Application periods:"}</span>
           <span className="">
             {
               " Apply by the monthly application deadline, and your project will be evaluated for rewards starting the following month."
@@ -75,13 +62,13 @@ export default function Header() {
           </span>
         </li>
         <li>
-          <span className="font-semibold ">{"Grant delivery:"}</span>
+          <span className="">{"Grant delivery:"}</span>
           <span className="">
             {" Rewards are delivered monthly, starting in March."}
           </span>
         </li>
         <li>
-          <span className="font-semibold ">{"Budget:"}</span>
+          <span className="">{"Budget:"}</span>
           <span className="">{" Up to 8M OP"}</span>
         </li>
       </ul>
