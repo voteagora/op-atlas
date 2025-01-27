fca93d5a70cfb199d9e6cbf56a4b7928b072be4a
Nikola Gigic <nik.gigic@gmail.com>
2025-01-27 14:12:16 +0100

    feat: task validation

--- app/src/app/api/tag-contacts/route.ts
+++ app/src/app/api/tag-contacts/route.ts
@@ -1,11 +1,31 @@
+import { NextRequest } from "next/server"
+
 export const dynamic = "force-dynamic"
 
-export function GET(request: Request) {
+export function GET(request: NextRequest) {
   if (
     request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
   ) {
     return new Response("Unauthorized", { status: 401 })
   }
 
-  return new Response(`Tagging Mailchimp contacts...`, { status: 200 })
+  const searchParams = request.nextUrl.searchParams
+
+  const task = searchParams.get("task")
+  if (task === "add") {
+    console.log("Adding tags to Mailchimp contacts...")
+
+    // Add tags to contacts
+
+    return new Response(`Mailchimp contacts tagged`, { status: 200 })
+  }
+  if (task === "remove") {
+    console.log("Removing tags from Mailchimp contacts...")
+
+    // Remove tags from contacts
+
+    return new Response(`Mailchimp contacts untagged`, { status: 200 })
+  }
+
+  return new Response(`Task not found`, { status: 404 })
 }
