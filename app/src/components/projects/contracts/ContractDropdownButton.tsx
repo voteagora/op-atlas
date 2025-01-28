cfb72f264f9bffb392c608120d16294e31480fa6
Jacob Homanics <homanicsjake@gmail.com>
2025-01-28 02:20:48 -0600

    improved dropdown functionality

--- app/src/components/projects/contracts/ContractDropdownButton.tsx
+++ app/src/components/projects/contracts/ContractDropdownButton.tsx
@@ -1,6 +1,6 @@
 import { FormField } from "@/components/ui/form"
 import { Ellipsis } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export function ContractDropdownButton({
   form,
@@ -11,15 +11,38 @@ export function ContractDropdownButton({
 }) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node) &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false)
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside)
+    }
+  }, [])
+
   return (
     <button
       type="button"
+      ref={buttonRef}
       className="flex items-center"
       onClick={() => setIsDropdownOpen(!isDropdownOpen)}
     >
       <Ellipsis width={16} height={16} />
       {isDropdownOpen && (
-        <div className="relative inline-block text-left">
+        <div ref={dropdownRef} className="relative inline-block text-left">
           <div className="absolute mt-2 -right-1 w-48 bg-white border rounded-md shadow-lg">
             <ul className="py-1 text-sm text-gray-700">
               <li>
