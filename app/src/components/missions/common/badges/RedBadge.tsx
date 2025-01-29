5b5a53f3391193115693f8837f4c788c8b594f19
Jacob Homanics <homanicsjake@gmail.com>
2025-01-29 04:50:15 -0600

    not eligibile / enrolled badge text increase

--- app/src/components/missions/common/badges/RedBadge.tsx
+++ app/src/components/missions/common/badges/RedBadge.tsx
@@ -11,7 +11,7 @@ export const RedBadge = ({
 }) => {
   return (
     <Badge
-      className={`text-xs font-medium text-rose-800 border-0 bg-rose-200`}
+      className={`text-sm font-medium text-rose-800 border-0 bg-rose-200`}
       variant={"outline"}
     >
       {showIcon && <XIcon width={12} height={12} />}
