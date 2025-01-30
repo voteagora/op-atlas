11789ee3552c46205ccbbec3230b8133b4d748e1
Jacob Homanics <homanicsjake@gmail.com>
2025-01-29 19:11:39 -0600

    Create page.tsx

---
+++ app/src/app/crispness/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <div>
+        <p>Hello</p>
+        <p className="font-bold">Hello</p>
+      </div>
+
+      <div>
+        <p className="font-light">Hello</p>
+        <p className="font-semibold">Hello</p>
+      </div>
+    </div>
+  )
+}
