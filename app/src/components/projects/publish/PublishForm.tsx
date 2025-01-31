cc42f7c200bba64a64a55152ad7ad46f585c3ca9
Jacob Homanics <homanicsjake@gmail.com>
2025-01-31 16:14:58 -0600

    sorted UI snapshots

--- app/src/components/projects/publish/PublishForm.tsx
+++ app/src/components/projects/publish/PublishForm.tsx
@@ -91,9 +91,16 @@ export const PublishForm = ({ project }: { project: ProjectWithDetails }) => {
         {project.snapshots.length > 0 ? (
           <div className="flex flex-col gap-2">
             <p className="text-sm  font-medium text-foreground">Published</p>
-            {sortBy((s) => -s.createdAt, project.snapshots).map((snapshot) => (
-              <Snapshot key={snapshot.id} snapshot={snapshot} />
-            ))}
+            {project.snapshots
+              .sort((a, b) => {
+                return (
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime()
+                )
+              })
+              .map((snapshot) => {
+                return <Snapshot key={snapshot.id} snapshot={snapshot} />
+              })}
 
             {/* Only show this when applications are open */}
             {/* {project.snapshots.length > 0 ? (
