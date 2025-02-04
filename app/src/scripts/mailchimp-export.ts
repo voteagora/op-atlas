6112ac6d654ea1e4cb3f5fc1365df5262accacb9
stepandel <stepandel@gmail.com>
2025-02-04 14:43:35 -0800

    mailchimp script nits

--- app/src/scripts/mailchimp-export.ts
+++ app/src/scripts/mailchimp-export.ts
@@ -8,6 +8,9 @@ type SUBSCRIBED_MEMBER = {
   subscriber_hash: string
   email_address: string
 }
+
+const LIST_ID = process.env.MAILCHIMP_LIST_ID
+
 async function exportEmailsToMailchimp() {
   const userEmails = await prisma.userEmail.findMany({
     // Do we need this?
@@ -25,31 +28,41 @@ async function exportEmailsToMailchimp() {
     },
   })
 
-  const batchMembers = userEmails.map((userEmail) => {
-    const userFullName = userEmail.user.name
-    const [FNAME, LNAME] = userFullName ? userFullName.split(" ") : ["", ""]
+  const batchMembers = userEmails
+    .map((userEmail) => {
+      const userFullName = userEmail.user.name
+      const [FNAME, LNAME] = userFullName ? userFullName.split(" ") : ["", ""]
 
-    const data: BaseMailchimp.lists.BatchListMembersBodyMembersObject = {
-      email_address: userEmail.email,
-      email_type: "html",
-      status: "subscribed",
-      merge_fields: {
-        EMAIL: userEmail.email,
-        FNAME,
-        LNAME,
-      },
-    }
+      const data: BaseMailchimp.lists.BatchListMembersBodyMembersObject = {
+        email_address: userEmail.email,
+        email_type: "html",
+        status: "subscribed",
+        merge_fields: {
+          EMAIL: userEmail.email,
+          FNAME,
+          LNAME,
+        },
+      }
 
-    return data
-  })
+      return data
+    })
+    .filter(
+      (member, index, self) =>
+        index ===
+        self.findIndex((t) => t.email_address === member.email_address),
+    )
 
   console.log(`Exporting ${batchMembers.length} emails to Mailchimp...`)
+
+  if (!LIST_ID) {
+    throw new Error("MAILCHIMP_LIST_ID is not set")
+  }
 
   try {
     for (let i = 0; i < batchMembers.length; i += BATCH_SIZE) {
       const batch = batchMembers.slice(i, i + BATCH_SIZE)
 
-      const res = await mailchimp.lists.batchListMembers("ae53f4c952", {
+      const res = await mailchimp.lists.batchListMembers(LIST_ID, {
         members: batch,
       })
 
