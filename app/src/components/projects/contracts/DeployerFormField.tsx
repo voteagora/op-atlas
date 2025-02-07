9c81f843f4a0e5c441453409e5eb59535f4d27eb
Jacob Homanics <homanicsjake@gmail.com>
2025-02-07 03:03:31 -0600

    verify button progress

--- app/src/components/projects/contracts/DeployerFormField.tsx
+++ app/src/components/projects/contracts/DeployerFormField.tsx
@@ -10,7 +10,9 @@ import {
 import { Input } from "@/components/ui/input"
 import { ContractsFormField } from "./ContractsFormField"
 import { Button } from "@/components/ui/button"
-import { Plus } from "lucide-react"
+import { Loader2, Plus } from "lucide-react"
+import { useState } from "react"
+import { isAddress } from "ethers"
 
 export function DeployerFormField({
   form,
@@ -19,6 +21,14 @@ export function DeployerFormField({
   form: UseFormReturn<z.infer<typeof DeployersSchema>>
   deployerIndex: number
 }) {
+  const [isVerifying, setIsVerifying] = useState(false)
+
+  async function onVerify() {
+    setIsVerifying(true)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    setIsVerifying(false)
+  }
+
   return (
     <FormField
       control={form.control}
@@ -29,29 +39,49 @@ export function DeployerFormField({
             control={form.control}
             name={`deployers.${deployerIndex}.address`}
             render={({ field: address }) => (
-              <FormItem className="flex flex-col gap-1.5">
-                <FormLabel>Deployer Address</FormLabel>
-                <Input {...address} placeholder="Add a deployer address" />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name={`deployers.${deployerIndex}.contracts`}
-            render={({ field: contracts }) => (
               <>
-                {contracts.value.length > 0 ? (
-                  <ContractsFormField
-                    form={form}
-                    deployerIndex={deployerIndex}
-                  />
-                ) : (
-                  <Button variant={"destructive"} className="w-20">
-                    Verify
-                  </Button>
-                )}
+                <FormItem className="flex flex-col gap-1.5">
+                  <FormLabel>Deployer Address</FormLabel>
+                  <Input {...address} placeholder="Add a deployer address" />
+                  <FormMessage />
+                </FormItem>
+
+                <FormField
+                  control={form.control}
+                  name={`deployers.${deployerIndex}.contracts`}
+                  render={({ field: contracts }) => (
+                    <>
+                      {contracts.value.length > 0 ? (
+                        <ContractsFormField
+                          form={form}
+                          deployerIndex={deployerIndex}
+                        />
+                      ) : (
+                        <>
+                          {isVerifying ? (
+                            <div className="flex items-center">
+                              <Loader2
+                                width={16}
+                                height={16}
+                                className="animate-spin"
+                              />
+                              <p>Searching for contracts</p>
+                            </div>
+                          ) : (
+                            <Button
+                              disabled={!isAddress(address.value)}
+                              variant={"destructive"}
+                              className="w-20"
+                              onClick={onVerify}
+                            >
+                              Verify
+                            </Button>
+                          )}
+                        </>
+                      )}
+                    </>
+                  )}
+                />
               </>
             )}
           />
