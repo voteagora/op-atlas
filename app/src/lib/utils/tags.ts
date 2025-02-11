0ba08338ba3a91bb8a498a0caf5d7270f0c320cc
Nikola Gigic <nik.gigic@gmail.com>
2025-02-11 16:53:28 +0100

    feat: keep existing mailchimp tags that dont exist in db

--- app/src/lib/utils/tags.ts
+++ app/src/lib/utils/tags.ts
@@ -35,3 +35,8 @@ export const fetchRecords = async (): Promise<EntityRecords> => {
 
   return result
 }
+
+export function arrayDifference(arr1: string[], arr2: string[]) {
+  const set2 = new Set(arr2)
+  return arr1.filter((item) => !set2.has(item))
+}
