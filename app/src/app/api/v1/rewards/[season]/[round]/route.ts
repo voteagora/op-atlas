d5f5a12abfa0dbd3024744322dc26d3a2832200e
stepandel <stepandel@gmail.com>
2025-03-25 11:05:54 -0700

    fix mock data

--- app/src/app/api/v1/rewards/[season]/[round]/route.ts
+++ app/src/app/api/v1/rewards/[season]/[round]/route.ts
@@ -18,7 +18,7 @@ const MOCK_REWARDS_TRACHE_1 = [
       "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["100000000000000000000"],
   },
   {
@@ -26,7 +26,7 @@ const MOCK_REWARDS_TRACHE_1 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: false,
+    KYCStatusCompleted: false,
     amounts: ["200000000000000000000"],
   },
   {
@@ -34,7 +34,7 @@ const MOCK_REWARDS_TRACHE_1 = [
       "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
     projectName: "Project 4",
     wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["400000000000000000000"],
   },
   {
@@ -42,7 +42,7 @@ const MOCK_REWARDS_TRACHE_1 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["500000000000000000000"],
   },
 ]
@@ -50,10 +50,10 @@ const MOCK_REWARDS_TRACHE_1 = [
 const MOCK_REWARDS_TRACHE_2 = [
   {
     projectId:
-      "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
+      "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["100000000000000000000", "100000000000000000000"],
   },
   {
@@ -61,15 +61,15 @@ const MOCK_REWARDS_TRACHE_2 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: false,
+    KYCStatusCompleted: false,
     amounts: ["200000000000000000000", "250000000000000000000"],
   },
   {
     projectId:
-      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
+      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a88",
     projectName: "Project 3",
-    wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: false,
+    wallets: ["0x7890123456789012345678901234567890abcdef"],
+    KYCStatusCompleted: false,
     amounts: ["0", "350000000000000000000"],
   },
   {
@@ -77,7 +77,7 @@ const MOCK_REWARDS_TRACHE_2 = [
       "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
     projectName: "Project 4",
     wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["400000000000000000000", "450000000000000000000"],
   },
   {
@@ -85,7 +85,7 @@ const MOCK_REWARDS_TRACHE_2 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["500000000000000000000", "550000000000000000000"],
   },
 ]
@@ -93,10 +93,10 @@ const MOCK_REWARDS_TRACHE_2 = [
 const MOCK_REWARDS_TRACHE_3 = [
   {
     projectId:
-      "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
+      "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "100000000000000000000",
       "100000000000000000000",
@@ -108,7 +108,7 @@ const MOCK_REWARDS_TRACHE_3 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "200000000000000000000",
       "250000000000000000000",
@@ -117,10 +117,10 @@ const MOCK_REWARDS_TRACHE_3 = [
   },
   {
     projectId:
-      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
+      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a88",
     projectName: "Project 3",
-    wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: false,
+    wallets: ["0x7890123456789012345678901234567890abcdef"],
+    KYCStatusCompleted: false,
     amounts: ["0", "350000000000000000000", "350000000000000000000"],
   },
   {
@@ -129,9 +129,9 @@ const MOCK_REWARDS_TRACHE_3 = [
     projectName: "Project 4",
     wallets: [
       "0x3456789012345678901234567890123456789012",
-      "0x3456789012345678901234567890123456789012",
+      "0x8901234567890123456789012345678901abcdef0",
     ],
-    KYCStautsCompleted: false,
+    KYCStatusCompleted: false,
     amounts: [
       "400000000000000000000",
       "450000000000000000000",
@@ -143,7 +143,7 @@ const MOCK_REWARDS_TRACHE_3 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["500000000000000000000", "250000000000000000000", "0"],
   },
 ]
@@ -151,10 +151,10 @@ const MOCK_REWARDS_TRACHE_3 = [
 const MOCK_REWARDS_TRACHE_4 = [
   {
     projectId:
-      "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
+      "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "100000000000000000000",
       "100000000000000000000",
@@ -167,7 +167,7 @@ const MOCK_REWARDS_TRACHE_4 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "200000000000000000000",
       "250000000000000000000",
@@ -177,10 +177,10 @@ const MOCK_REWARDS_TRACHE_4 = [
   },
   {
     projectId:
-      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
+      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a88",
     projectName: "Project 3",
-    wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: true,
+    wallets: ["0x7890123456789012345678901234567890abcdef"],
+    KYCStatusCompleted: true,
     amounts: [
       "0",
       "350000000000000000000",
@@ -194,9 +194,9 @@ const MOCK_REWARDS_TRACHE_4 = [
     projectName: "Project 4",
     wallets: [
       "0x3456789012345678901234567890123456789012",
-      "0x3456789012345678901234567890123456789012",
+      "0x8901234567890123456789012345678901abcdef0",
     ],
-    KYCStautsCompleted: false,
+    KYCStatusCompleted: false,
     amounts: [
       "400000000000000000000",
       "450000000000000000000",
@@ -209,7 +209,7 @@ const MOCK_REWARDS_TRACHE_4 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["500000000000000000000", "250000000000000000000", "0", "0"],
   },
 ]
@@ -217,10 +217,10 @@ const MOCK_REWARDS_TRACHE_4 = [
 const MOCK_REWARDS_TRACHE_5 = [
   {
     projectId:
-      "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
+      "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "100000000000000000000",
       "100000000000000000000",
@@ -234,7 +234,7 @@ const MOCK_REWARDS_TRACHE_5 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "200000000000000000000",
       "250000000000000000000",
@@ -245,10 +245,10 @@ const MOCK_REWARDS_TRACHE_5 = [
   },
   {
     projectId:
-      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
+      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a88",
     projectName: "Project 3",
-    wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: true,
+    wallets: ["0x7890123456789012345678901234567890abcdef"],
+    KYCStatusCompleted: true,
     amounts: [
       "0",
       "350000000000000000000",
@@ -263,9 +263,9 @@ const MOCK_REWARDS_TRACHE_5 = [
     projectName: "Project 4",
     wallets: [
       "0x3456789012345678901234567890123456789012",
-      "0x3456789012345678901234567890123456789012",
+      "0x8901234567890123456789012345678901abcdef0",
     ],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "400000000000000000000",
       "450000000000000000000",
@@ -279,7 +279,7 @@ const MOCK_REWARDS_TRACHE_5 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: ["500000000000000000000", "250000000000000000000", "0", "0", "0"],
   },
 ]
@@ -287,10 +287,10 @@ const MOCK_REWARDS_TRACHE_5 = [
 const MOCK_REWARDS_TRACHE_6 = [
   {
     projectId:
-      "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
+      "0x9aa8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a84",
     projectName: "Project 1",
     wallets: ["0x1234567890123456789012345678901234567890"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "100000000000000000000",
       "100000000000000000000",
@@ -305,7 +305,7 @@ const MOCK_REWARDS_TRACHE_6 = [
       "0x9bb8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a85",
     projectName: "Project 2",
     wallets: ["0x2345678901234567890123456789012345678901"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "200000000000000000000",
       "250000000000000000000",
@@ -317,10 +317,10 @@ const MOCK_REWARDS_TRACHE_6 = [
   },
   {
     projectId:
-      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a86",
+      "0x9cc8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a88",
     projectName: "Project 3",
-    wallets: ["0x3456789012345678901234567890123456789012"],
-    KYCStautsCompleted: true,
+    wallets: ["0x7890123456789012345678901234567890abcdef"],
+    KYCStatusCompleted: true,
     amounts: [
       "0",
       "350000000000000000000",
@@ -336,9 +336,9 @@ const MOCK_REWARDS_TRACHE_6 = [
     projectName: "Project 4",
     wallets: [
       "0x3456789012345678901234567890123456789012",
-      "0x3456789012345678901234567890123456789012",
+      "0x8901234567890123456789012345678901abcdef0",
     ],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "400000000000000000000",
       "450000000000000000000",
@@ -353,7 +353,7 @@ const MOCK_REWARDS_TRACHE_6 = [
       "0x9dd8d3f8aa6793dba4176f706571c16650bb6343bb7c56ac9dd249d6db3b2a87",
     projectName: "Project 5",
     wallets: ["0x4567890123456789012345678901234567890123"],
-    KYCStautsCompleted: true,
+    KYCStatusCompleted: true,
     amounts: [
       "500000000000000000000",
       "250000000000000000000",
