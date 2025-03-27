cc99de4667400a4dc640fce70a8056c5b6c39df9
Nikola Gigic <nik.gigic@gmail.com>
2025-03-27 14:52:32 +0100

    fix: env var

--- app/src/lib/utils/apiFetch.ts
+++ app/src/lib/utils/apiFetch.ts
@@ -2,7 +2,7 @@ export default async function apiFetch(
   endpoint: string,
   opts: RequestInit = {},
 ) {
-  const API_URL = process.env.NEXT_PUBLIC_URL
+  const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL
 
   return await fetch(`${API_URL}/${endpoint}`, opts)
 }
