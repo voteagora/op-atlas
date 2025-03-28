277189d45bc58d7f7f0644137ca8ccfbc17e6d81
Nikola Gigic <nik.gigic@gmail.com>
2025-03-28 15:36:17 +0100

    feat: populate dev-tooling

---
+++ app/src/scripts/populate-dev-tooling-rewards.ts
@@ -0,0 +1,30 @@
+import { v4 as uuidV4 } from "uuid"
+
+import devTooling from "@/data/dev-tooling.json"
+import { prisma } from "@/db/client"
+import { chunkArray } from "@/lib/utils"
+
+const BATCH_SIZE = 10
+
+const populateRecipientsRewards = async () => {
+  console.log("📦 Preparing data")
+  const rewards = devTooling.map((item) => {
+    return {
+      id: uuidV4(),
+      projectId: item.project_name,
+      amount: item.op_reward ?? 0,
+      roundId: item.round_id,
+    }
+  })
+  console.log("🚀 Inserting data")
+  const batches = chunkArray(rewards, BATCH_SIZE)
+  for (const batch of batches) {
+    await prisma.fundingReward.createMany({
+      data: batch,
+      skipDuplicates: true,
+    })
+  }
+  console.log("✅ Done")
+}
+
+populateRecipientsRewards()
