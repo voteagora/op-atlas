447ce168d5066010937ffc5179d63a81fd86991d
Nikola Gigic <nik.gigic@gmail.com>
2025-03-31 22:45:13 +0200

    fix: increase your impact isMember check

--- app/src/app/project/[projectId]/page.tsx
+++ app/src/app/project/[projectId]/page.tsx
@@ -176,35 +176,35 @@ export default async function Page({ params }: PageProps) {
                   )}
                 </ul>
               </div>
-              <div className="w-full space-y-6">
-                <div className="flex items-center space-x-2 group">
-                  <h4 className="font-semibold text-xl">
-                    Increase your impact
-                  </h4>
-                  <button>
-                    <EyeOff
-                      size={20}
-                      className="opacity-0 group-hover:opacity-100 transition-all duration-150"
-                    />
-                  </button>
+              {publicProjectMetrics.isMember && (
+                <div className="w-full space-y-6">
+                  <div className="flex items-center space-x-2 group">
+                    <h4 className="font-semibold text-xl">
+                      Increase your impact
+                    </h4>
+                    <button>
+                      <EyeOff
+                        size={20}
+                        className="opacity-0 group-hover:opacity-100 transition-all duration-150"
+                      />
+                    </button>
+                  </div>
+                  <div
+                    className={cn([
+                      "gap-4 grid",
+                      {
+                        "grid-cols-2": isOnchainBuilder && isDevTooling,
+                        "grid-cols-1": isOnchainBuilder || isDevTooling,
+                      },
+                    ])}
+                  >
+                    {isOnchainBuilder && (
+                      <IncreaseYourImpact type="onchain-builders" />
+                    )}
+                    {isDevTooling && <IncreaseYourImpact type="dev-tooling" />}
+                  </div>
                 </div>
-                <div
-                  className={cn([
-                    "gap-4 grid",
-                    {
-                      "grid-cols-2": isOnchainBuilder && isDevTooling,
-                      "grid-cols-1": isOnchainBuilder || isDevTooling,
-                    },
-                  ])}
-                >
-                  {publicProjectMetrics.isMember && isOnchainBuilder && (
-                    <IncreaseYourImpact type="onchain-builders" />
-                  )}
-                  {publicProjectMetrics.isMember && isDevTooling && (
-                    <IncreaseYourImpact type="dev-tooling" />
-                  )}
-                </div>
-              </div>
+              )}
             </>
           )}
           {/* TODO: Bring this back later */}
