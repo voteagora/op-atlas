eaa20728dc7093a7dd9d917c730baac75f62665a
Stepan Arsentjev <stepan@voteagora.com>
2025-04-07 16:35:28 -0700

    Deprecate old claim flow (#783)
    * deprecate old claim flow
    
    * redirect user to project profile instead of reward claim
    
    * for older rewards send to rewards page
    
    * show rewards dialog

--- app/src/app/rewards/[rewardId]/page.tsx
+++ app/src/app/rewards/[rewardId]/page.tsx
@@ -1,40 +1,9 @@
 import { redirect } from "next/navigation"
 
-import { auth } from "@/auth"
-import RewardClaimFlow from "@/components/rewards/RewardClaimFlow"
-import { getReward } from "@/db/rewards"
-import { verifyAdminStatus } from "@/lib/actions/utils"
-
 export default async function Page({
   params,
 }: {
   params: { rewardId: string }
 }) {
-  const session = await auth()
-  const claim = await getReward({ id: params.rewardId })
-
-  if (!claim || !session) {
-    redirect("/dashboard")
-  }
-
-  const isInvalid = await verifyAdminStatus(
-    claim.projectId,
-    session.user.farcasterId,
-  )
-
-  if (isInvalid?.error) {
-    redirect("/dashboard")
-  }
-
-  const isUserAdmin = !isInvalid?.error
-
-  return (
-    <main className="flex flex-col flex-1 h-full items-center bg-secondary pb-12">
-      <RewardClaimFlow
-        className="mt-18 max-w-4xl"
-        reward={claim}
-        isUserAdmin={isUserAdmin}
-      />
-    </main>
-  )
+  redirect("/dashboard")
 }
