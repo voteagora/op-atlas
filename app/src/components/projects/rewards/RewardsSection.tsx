3915eda870a38690e1cc9e177eb0f77c5221c708
Nikola Gigić <nik.gigic@gmail.com>
2025-04-08 00:46:58 +0200

    fix: issue-728 rework kyc component UI (#779). closes #728
    * fix: issue-728 rework kyc component UI
    
    * feat: start over
    
    * remove reward addresses from rewards page
    
    * hide new grant addresses tab behind feature flag
    
    ---------
    
    Co-authored-by: stepandel <stepandel@gmail.com>

--- app/src/components/projects/rewards/RewardsSection.tsx
+++ app/src/components/projects/rewards/RewardsSection.tsx
@@ -1,14 +1,7 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query"
-import { useFeatureFlagEnabled } from "posthog-js/react"
-import React from "react"
-
-import ExtendedLink from "@/components/common/ExtendedLink"
-import { getKycTeamAction } from "@/lib/actions/projects"
 import { ProjectTeam, ProjectWithFullDetails } from "@/lib/types"
 
-import AddGrantDeliveryAddressForm from "./AddGrantDeliveryAddressForm"
 import RewardAccordion from "./RewardAccordion"
 
 export function RewardsSection({
@@ -18,15 +11,6 @@ export function RewardsSection({
   project: ProjectWithFullDetails & { organizationId?: string }
   team: ProjectTeam
 }) {
-  const isKycEnabled = useFeatureFlagEnabled("add-grant-delivery-address-form")
-
-  const { data: kycTeam, isLoading } = useQuery({
-    queryKey: ["kyc-teams", "project", project.id],
-    queryFn: async () => {
-      return await getKycTeamAction(project.id)
-    },
-    enabled: isKycEnabled,
-  })
   const rewards = project.rewards.filter(
     (reward) => parseInt(reward.roundId) < 7, // Don't show Season 7 yet
   )
@@ -50,38 +34,6 @@ export function RewardsSection({
           </span>
         )}
       </div>
-      {isKycEnabled && (
-        <div className="flex flex-col space-y-6">
-          <h2>Grant Delivery Address</h2>
-          <p className="text-secondary-foreground">
-            Add the address(es) your rewards will be delivered to. You can do
-            this at any time, and your entry will be valid for one year.
-          </p>
-          <p className="text-secondary-foreground">
-            KYC (identity verification) is required for each address.
-          </p>
-          {Boolean(project.organization) ? (
-            <ExtendedLink
-              as="button"
-              href={`/profile/organizations/${project.organizationId}/grant-address`}
-              text="Go to organization settings"
-              variant="primary"
-              target="_self"
-            />
-          ) : isLoading ? (
-            <div className="p-6 border rounded-md space-y-6 w-full h-[356px]">
-              <div className="animate-pulse bg-gray-200 rounded-md h-8 w-full" />
-              <div className="space-y-4">
-                <div className="animate-pulse bg-gray-200 rounded-md h-[146px] w-full" />
-                <div className="animate-pulse bg-gray-200 rounded-md h-8 w-full" />
-                <div className="animate-pulse bg-gray-200 rounded-md h-8 w-full" />
-              </div>
-            </div>
-          ) : (
-            <AddGrantDeliveryAddressForm kycTeam={kycTeam} />
-          )}
-        </div>
-      )}
     </div>
   )
 }
