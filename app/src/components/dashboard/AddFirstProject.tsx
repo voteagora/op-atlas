dc105e05e2d42ba65ba29ed65d16fdc33f80f898
andrei <andrei.taraschuk@gmail.com>
2025-04-09 16:39:22 -0600

    Icon alignment  (#794)
    * Empty profile card alignment #292
    
    * Styling cleanup

--- app/src/components/dashboard/AddFirstProject.tsx
+++ app/src/components/dashboard/AddFirstProject.tsx
@@ -1,4 +1,4 @@
-import { Plus } from "lucide-react"
+import Image from "next/image"
 
 import { cn } from "@/lib/utils"
 
@@ -6,12 +6,12 @@ const AddFirstProject = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "flex items-center gap-6 border rounded-xl p-8 select-none hover:shadow-sm",
+        "flex gap-4 items-center border rounded-xl p-8 select-none hover:shadow-sm",
         className,
       )}
     >
-      <div className="flex items-center justify-center border border-dashed rounded-xl h-12 w-12">
-        <Plus height={11.6} width={11.6} className="text-text-default" />
+      <div className="flex items-center justify-center border border-dashed border-muted rounded-xl h-12 w-12">
+        <Image src="/assets/icons/plus.svg" width={20} height={20} alt="Plus" />
       </div>
       <h3 className="text-base font-semibold">Add your first project</h3>
     </div>
