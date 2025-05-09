e0bfdbadcd7bdadcde0f958046d3eb6946e0b456
andrei <andrei.taraschuk@gmail.com>
2025-05-09 12:45:17 -0600

    privy csp

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -4,6 +4,30 @@ const nextConfig = {
   headers: async () => {
     return [
       {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' https://challenges.cloudflare.com;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: blob:;
+              font-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              child-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org;
+              frame-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
+              connect-src 'self' https://auth.privy.io wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://explorer-api.walletconnect.com;
+              worker-src 'self';
+              manifest-src 'self'
+            `,
+          },
+        ],
+      },
+      {
         source: "/api/tag-contacts",
         headers: [
           {
