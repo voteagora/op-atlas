eeefa10133c45fee248fd9baf00a5df902ab04ae
andrei <andrei.taraschuk@gmail.com>
2025-05-22 14:10:42 -0600

    Implemented world id connection

--- app/src/components/profile/WorldIdConnection.tsx
+++ app/src/components/profile/WorldIdConnection.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import { Button } from '@/components/common/Button';
 import { useWorldIdVerification } from '@/hooks/useWorldIdVerification';
 import { IDKitWidget } from '@worldcoin/idkit';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 interface Props {
     userId: string;
     children?: React.ReactNode;
+    className?: string;
 }
 
-export function WorldConnection({ userId, children }: Props) {
+export function WorldConnection({ userId, children, className }: Props) {
     const [isClient, setIsClient] = useState(false);
     const [isVerified, setIsVerified] = useState(false);
     const { isVerifying, error, verifyProof } = useWorldIdVerification();
@@ -40,34 +39,15 @@ export function WorldConnection({ userId, children }: Props) {
     };
 
     return (
-        <div className="flex flex-row gap-2">
-            {isVerified && (
-                <div className="flex flex-col gap-2">
-                    <div className="flex items-center gap-1.5">
-                        <div className="flex flex-1 p-3 border items-center gap-1.5 rounded-lg h-10">
-                            <Image
-                                src="/assets/icons/circle-check-green.svg"
-                                height={16.67}
-                                width={16.67}
-                                alt="Verified"
-                            />
-                            <p className="text-sm">Verified with World ID</p>
-                        </div>
-                    </div>
-                </div>
+        <IDKitWidget
+            app_id={process.env.NEXT_PUBLIC_WORLD_APP_ID!}
+            action="atals-humanity-verification"
+            onSuccess={handleSuccess}
+        >
+            {({ open }: { open: () => void }) => (
+                <div className={className} onClick={open} >{children}</div>
             )}
+        </IDKitWidget>
 
-            {!isVerified && (
-                <IDKitWidget
-                    app_id={process.env.NEXT_PUBLIC_WORLD_APP_ID!}
-                    action="atals-humanity-verification"
-                    onSuccess={handleSuccess}
-                >
-                    {({ open }: { open: () => void }) => (
-                        <Button onClick={open} variant="primary">{children}</Button>
-                    )}
-                </IDKitWidget>
-            )}
-        </div>
     );
 } 
