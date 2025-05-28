e21e925c4ad87365b4eba905c1044a783f1cf7a9
andrei <andrei.taraschuk@gmail.com>
2025-05-28 14:30:46 -0600

    Finalized dialogs

--- app/src/app/citizenship/enroll/components/CitizenshipRegistrationSidebar.tsx
+++ app/src/app/citizenship/enroll/components/CitizenshipRegistrationSidebar.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { useState } from "react"
 
 import { UserAvatarLarge } from "@/components/common/UserAvatarLarge"
+import CitizenshipRulesDialog from "@/components/dialogs/CitizenshipRulesDialog"
 import { Button } from "@/components/ui/button"
 import { useCitizenshipRequirements } from "@/hooks/useCitizenshipRequirements"
 import { User } from "@prisma/client"
 
 export const CitizenshipRegistrationSidebar = ({ user }: { user: User }) => {
     const isEligible = useCitizenshipRequirements({ id: user.id })
+    const [isRulesDialogOpen, setIsRulesDialogOpen] = useState(false)
 
     return (
         <div className="w-full flex flex-col text-center items-center gap-6 border border-border-secondary rounded-lg p-6">
@@ -18,9 +21,18 @@ export const CitizenshipRegistrationSidebar = ({ user }: { user: User }) => {
                 <div className="text-sm text-destructive"> Complete your registration requirements in order to continue</div>
             }
 
-            <Button className="w-full button-primary" disabled={!isEligible}>
+            <Button
+                className="w-full button-primary"
+                disabled={!isEligible}
+                onClick={() => setIsRulesDialogOpen(true)}
+            >
                 Register
             </Button>
+
+            <CitizenshipRulesDialog
+                open={isRulesDialogOpen}
+                onOpenChange={setIsRulesDialogOpen}
+            />
         </div >
     )
 }
