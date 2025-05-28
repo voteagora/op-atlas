55128b790e9de49e82de052ff8d2ab03b5861527
daktic <garrett@voteagora.com>
2025-05-28 18:47:15 -0400

    This somehow is semi stable

--- app/src/app/proposals/[proposalId]/page.tsx
+++ app/src/app/proposals/[proposalId]/page.tsx
@@ -1,4 +1,16 @@
 import { notFound } from "next/navigation"
+import { auth } from "@/auth"
+import { getPublicProjectAction } from "@/lib/actions/projects"
+import { getProjectMetrics } from "@/lib/oso"
+import Markdown from "@/components/common/Markdown/Markdown"
+
+function stripTitleFromDescription(title: string, description: string) {
+  if (description.startsWith(`# ${title}`)) {
+    const newDescription = description.slice(`# ${title}`.length).trim()
+    return newDescription
+  }
+  return description
+}
 
 interface PageProps {
   params: {
@@ -9,13 +21,17 @@ interface PageProps {
 const Page = (params: PageProps) => {
   // Get the proposals page
 
-  if (!false) {
+  const { proposalId } = params.params
+
+  const proposalIdData = true
+
+  if (!proposalIdData) {
     return notFound()
   }
 
   return (
     <main className="flex flex-col flex-1 h-full items-center bg-secondary pb-12">
-      Test
+      <Markdown content={stripTitleFromDescription("", "")} />
     </main>
   )
 }
---
+++ app/src/lib/tenants/configs/ui/optimism.ts
@@ -0,0 +1,329 @@
+import { TenantUI } from "@/lib/tenants/tenantUI"
+import optimismLogo from "@/assets/optimism/optimism_logo.svg"
+import optimismHero from "@/assets/optimism/optimism_hero.svg"
+import successImage from "@/assets/optimism/optimism_success.svg"
+import pendingImage from "@/assets/optimism/optimism_pending.svg"
+import delegateImage from "@/assets/optimism/optimism_delegate.svg"
+import infoPageCard01 from "@/assets/optimism/optimism_info_1.png"
+import infoPageCard02 from "@/assets/optimism/optimism_info_2.png"
+import infoPageCard03 from "@/assets/optimism/optimism_info_3.png"
+import infoPageCard04 from "@/assets/optimism/optimism_info_4.png"
+import { ProposalGatingType } from "@/lib/types"
+import { ProposalStage as PrismaProposalStage } from "@/lib/types"
+import { TENANT_NAMESPACES } from "@/lib/constants"
+
+export const optimismTenantUIConfig = new TenantUI({
+  title: "Optimism Agora",
+  logo: optimismLogo,
+
+  assets: {
+    success: successImage,
+    pending: pendingImage,
+    delegate: delegateImage,
+  },
+
+  customization: {
+    primary: "0 0 0",
+    secondary: "64 64 64",
+    tertiary: "115 115 115",
+    neutral: "255 255 255",
+    wash: "250 250 250",
+    line: "229 229 229",
+    positive: "97 209 97",
+    negative: "226 54 54",
+    brandPrimary: "0 0 0",
+    brandSecondary: "255 255 255",
+    tokenAmountFont: "font-chivoMono",
+  },
+
+  delegates: {
+    allowed: [],
+    advanced: [],
+    retired: [
+      "0x5e349eca2dc61abcd9dd99ce94d04136151a09ee", // lindajxie.eth
+    ],
+  },
+
+  governanceIssues: [
+    {
+      icon: "banknotes",
+      title: "Treasury management",
+      key: "treasury",
+    },
+    {
+      icon: "piggyBank",
+      title: "Grant funding",
+      key: "funding",
+    },
+    {
+      icon: "sparks",
+      title: "Public goods",
+      key: "publicGoods",
+    },
+  ],
+
+  organization: {
+    title: "Optimism Foundation",
+  },
+
+  links: [
+    {
+      name: "calendar",
+      title: "Governance calendar",
+      url: "https://calendar.google.com/calendar/ical/c_fnmtguh6noo6qgbni2gperid4k%40group.calendar.google.com/public/basic.ics",
+    },
+    {
+      name: "faq",
+      title: "FAQ",
+      url: "https://agoraxyz.notion.site/Optimism-Agora-FAQ-3922ac9c66e54a21b5de16be9e0cf79c",
+    },
+    {
+      name: "advanced-delegation-faq",
+      title: "advanced delegation FAQ",
+      url: "https://agoraxyz.notion.site/Optimism-Agora-FAQ-3922ac9c66e54a21b5de16be9e0cf79c",
+    },
+    {
+      name: "changelog",
+      title: "Change log",
+      url: "/changelog",
+    },
+    {
+      name: "discord",
+      title: "Discord",
+      url: "https://discord.gg/vBJkUYBuwX",
+    },
+    {
+      name: "bugs",
+      title: "Report bugs & feedback",
+      url: "https://app.deform.cc/form/7180b273-7662-4f96-9e66-1eae240a52bc",
+    },
+    {
+      name: "governance-forum",
+      title: "Governance Forum",
+      url: "https://gov.optimism.io/",
+    },
+    {
+      name: "code-of-conduct",
+      title: "Delegate Code of Conduct",
+      url: "https://gov.optimism.io/t/code-of-conduct/5751",
+    },
+    {
+      name: "delegate-statement-template",
+      title: "View Template",
+      url: "https://gov.optimism.io/t/delegate-commitments/235",
+    },
+  ],
+
+  pages: [
+    {
+      route: "/",
+      title: "Agora is the home of Optimism voters",
+      description:
+        "OP Delegates are the stewards of the Optimism Token House, appointed by token holders to make governance decisions on their behalf.",
+      hero: optimismHero,
+      meta: {
+        title: "Optimism Agora",
+        description: "Home of token house governance and RPGF",
+        imageTitle: "Optimism Agora",
+        imageDescription: "Home of token house governance and RPGF",
+      },
+    },
+    {
+      route: "proposals",
+      title: "Agora is the home of Optimism voters",
+      description:
+        "OP Delegates are the stewards of the Optimism Token House, appointed by token holders to make governance decisions on their behalf.",
+      hero: optimismHero,
+      meta: {
+        title: "Optimism Agora",
+        description: "Home of token house governance and RPGF",
+        imageTitle: "Optimism Agora",
+        imageDescription: "Home of token house governance and RPGF",
+      },
+    },
+    {
+      route: "delegates",
+      title: "Agora is the home of Optimism voters",
+      description:
+        "OP Delegates are the stewards of the Optimism Token House, appointed by token holders to make governance decisions on their behalf.",
+      hero: optimismHero,
+      meta: {
+        title: "Voter on Agora",
+        description: "Delegate your voting power to a trusted representative",
+        imageTitle: "Voter on Agora",
+        imageDescription:
+          "Delegate your voting power to a trusted representative",
+      },
+    },
+    {
+      route: "info",
+      title: "Welcome to the Optimism Collective",
+      description:
+        "A collective of companies, communities, and citizens working together.",
+      hero: optimismHero,
+      meta: {
+        title: "Info of Agora",
+        description: "Welcome to the Optimism Collective",
+        imageTitle: "Info of Agora",
+        imageDescription: "Welcome to the Optimism Collective",
+      },
+      links: [
+        {
+          name: "Community Discord",
+          title: "Community Discord",
+          url: "",
+          image: infoPageCard01,
+        },
+        {
+          name: "Governance Forums",
+          title: "Governance Forums",
+          url: "",
+          image: infoPageCard02,
+        },
+        {
+          name: "Protocol Docs",
+          title: "Protocol Docs",
+          url: "",
+          image: infoPageCard03,
+        },
+        {
+          name: "Optimistic Vision",
+          title: "Optimistic Vision",
+          url: "",
+          image: infoPageCard04,
+        },
+      ],
+    },
+  ],
+
+  toggles: [
+    {
+      name: "admin",
+      enabled: true,
+    },
+    {
+      name: "proposals",
+      enabled: true,
+    },
+    {
+      name: "proposals/create",
+      enabled: true,
+    },
+    {
+      name: "delegates",
+      enabled: true,
+    },
+    {
+      name: "delegates/code-of-conduct",
+      enabled: true,
+    },
+    {
+      name: "retropgf",
+      enabled: false,
+    },
+    {
+      name: "citizens",
+      enabled: true,
+    },
+    {
+      name: "delegates/edit",
+      enabled: true,
+    },
+    {
+      name: "info",
+      enabled: false,
+    },
+    {
+      name: "proposal-execute",
+      enabled: true,
+    },
+    {
+      name: "email-subscriptions",
+      enabled: true,
+    },
+    {
+      name: "proposal-lifecycle",
+      enabled: true,
+      config: {
+        stages: [
+          {
+            stage: PrismaProposalStage.DRAFTING,
+            order: 0,
+            isPreSubmission: true,
+          },
+          {
+            stage: PrismaProposalStage.AWAITING_SUBMISSION,
+            order: 1,
+            isPreSubmission: true,
+          },
+          {
+            stage: PrismaProposalStage.PENDING,
+            order: 2,
+            isPreSubmission: false,
+          },
+          {
+            stage: PrismaProposalStage.QUEUED,
+            order: 3,
+            isPreSubmission: false,
+          },
+          {
+            stage: PrismaProposalStage.EXECUTED,
+            order: 4,
+            isPreSubmission: false,
+          },
+        ],
+        proposalTypes: [
+          {
+            type: "basic",
+            prodAddress: null,
+            testnetAddress: null,
+          },
+          {
+            type: "approval",
+            prodAddress: "0x8060B18290F48fc0bF2149EEb2F3c280bDe7674f",
+            testnetAddress: "0xdd0229D72a414DC821DEc66f3Cc4eF6dB2C7b7df",
+          },
+          {
+            type: "optimistic",
+            prodAddress: "0x8980C97f0e8a3A69831139e51003E65238F1F343",
+            testnetAddress: "0x4ab438e093647fC5eC7B773D1A5f6d3C2C7F0593",
+          },
+        ],
+        copy: {
+          helperText: `
+## Proposal checklist
+
+**1. Select proposal type**
+
+Proposal types set the quorum and approval thresholds for your proposal. You can view, edit, or create a new one via the [admin panel](https://vote.optimism.io/admin).
+
+**2. Choose your vote type**
+
+This determines if your proposal will be a simple yes/no or a multiple choice.
+
+**3. Create your proposal draft**
+
+Now that the vote and proposal type are set, you can use this form to create your proposal. Proposed transactions are optional, as the Token House governor is not executable for now.
+
+**4. Get signatures for your SAFE**
+
+If you're using the OP Foundation multisig, you can queue several proposals at once so that your co-signers can sign all the transactions in one sitting. Proposals will appear in chronological order in the final UI, so the last proposal you put in will show up on top for voters. Note that the order is not guaranteed if you batch all the proposal creation transactions into a single block, as then there is no timing difference.
+`.trim(),
+        },
+        gatingType: ProposalGatingType?.MANAGER,
+      },
+    },
+    {
+      name: "delegation-encouragement",
+      enabled: true,
+    },
+    {
+      name: "use-daonode-for-proposals",
+      enabled: false,
+    },
+    {
+      name: "use-daonode-for-votable-supply",
+      enabled: false,
+    },
+  ],
+})
