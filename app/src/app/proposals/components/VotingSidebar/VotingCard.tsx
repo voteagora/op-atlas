cba3f8d5f22355af9705121998543f0ec4600e69
daktic <garrett@voteagora.com>
2025-05-29 10:53:45 -0400

    Added components of Card Image into a React component

--- app/src/app/proposals/components/VotingSidebar/VotingCard.tsx
+++ app/src/app/proposals/components/VotingSidebar/VotingCard.tsx
@@ -1,5 +1,67 @@
-const votingCard = () => {
-  return <></>
+interface CardImageProps {
+  src: string
+  alt: string
 }
 
-export default votingCard
+const CardImage = ({ src, alt }: CardImageProps) => {
+  return (
+    <div>
+      <img src={src} alt={alt} />
+    </div>
+  )
+}
+
+interface CardTextProps {
+  title: string
+  description: string
+}
+
+const CardText = ({ title, description }: CardTextProps) => {
+  return (
+    <div>
+      <h4 className="text-h4">{title}</h4>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+interface CardActionsProps {
+  cardActions: CardAction[]
+}
+
+interface CardAction {
+  buttonStyle: string
+  actionText: string
+  onClick: () => void
+}
+
+const CardActions = ({ cardActions }: CardActionsProps) => {
+  return (
+    <div>
+      {cardActions.map(({ buttonStyle, actionText, onClick }, idx) => (
+        <button key={idx} onClick={onClick}>
+          {actionText}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export interface VotingCardProps {
+  cardText: CardTextProps
+  cardActions?: CardActionsProps
+  cardImage?: CardImageProps
+}
+
+const VotingCard = ({ cardText, cardActions, cardImage }: VotingCardProps) => {
+  return (
+    <div>
+      {cardImage && <CardImage {...cardImage} />}
+      <CardText {...cardText} />
+      {cardActions && <CardActions {...cardActions} />}
+    </div>
+  )
+}
+
+export default VotingCard
+export { CardText, type CardTextProps }
--- app/src/app/proposals/components/VotingSidebar/VotingSidebar.tsx
+++ app/src/app/proposals/components/VotingSidebar/VotingSidebar.tsx
@@ -1,15 +1,21 @@
 import React from "react"
-import VotingCard from "@/app/proposals/components/VotingSidebar/VotingCard"
+import VotingCard, {
+  VotingCardProps,
+} from "@/app/proposals/components/VotingSidebar/VotingCard"
 import VotingColumn from "@/app/proposals/components/VotingSidebar/VotingColumn"
 import VotingRedirect from "@/app/proposals/components/VotingSidebar/VotingRedirect"
 
 interface VotingSidebarProps {
   className?: string
+  votingCardProps: VotingCardProps
 }
 
-const VotingSidebar = ({ className = "" }: VotingSidebarProps) => (
+const VotingSidebar = ({
+  className = "",
+  votingCardProps,
+}: VotingSidebarProps) => (
   <div className={`flex flex-col ${className}`}>
-    <VotingCard />
+    <VotingCard {...votingCardProps} />
     <VotingColumn />
     <VotingRedirect />
   </div>
