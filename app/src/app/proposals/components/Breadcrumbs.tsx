e1c0f6abf908d0365bc29234cc664dad07601762
daktic <garrett@voteagora.com>
2025-05-29 10:09:18 -0400

    Updated Styling

--- app/src/app/proposals/components/Breadcrumbs.tsx
+++ app/src/app/proposals/components/Breadcrumbs.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface BreadcrumbsProps {
   value: string;
+  className?: string;
 }
 
-const Breadcrumbs = ({ value }: BreadcrumbsProps) => (
-  <div className="flex gap-4">{value}</div>
+const Breadcrumbs = ({ value, className = '' }: BreadcrumbsProps) => (
+  <div className={`flex gap-4 ${className}`}>{value}</div>
 );
 
 export default Breadcrumbs;
