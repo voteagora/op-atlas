7493c6651094de2cf5d92a3405d8df7cd275bad6
Garrett Berg <58493066+Daktic@users.noreply.github.com>
2025-05-30 14:58:26 -0400

    Merge pull request #941
    * wrenching on proposals page
    
    * We have the basic structure with minimal styling for the standard pro…
    
    * Having some trouble with map ordering of boxes
    
    * Styling updated to text content
    
    * Styling updated to enforce spacing between proposal card children
    
    * Updated proposal badge styling
    
    * Updated proposal card styling
    
    * Updated proposal text content styling
    
    * Updated proposal date styling
    
    * Made some color updates to the components. Need to ask about the bacg…
    
    * more responsive styling
    
    * Fixed issue where text was floating
    
    * Allowed dates to fit oin box
    
    * Added the Governance H1
    
    * Added styling for text i nh4
    
    * Templatized proposals for re-use
    
    * Adding a second proposal section
    
    * Adding mock proposals for self appointed section
    
    * Updated bade type options
    
    * Updated bade styles
    
    * Fixed bug with rounding top stackable
    
    * Fixed bacground color issue

---
+++ app/src/app/proposals/proposalsPage/components/ProposalCard.tsx
@@ -0,0 +1,117 @@
+type ProposalCardProps = {
+  children: React.ReactNode
+  rounded?: boolean
+}
+
+const ProposalCard = ({ children, rounded = false }: ProposalCardProps) => {
+  return (
+    <div
+      className={`proposal-card-container p-6 border border-border ${
+        rounded ? "rounded-t-lg" : ""
+      }`}
+    >
+      <div className="proposal-card-content flex flex-row gap-2 justify-between items-center">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export enum ProposalBadgeType {
+  soon = "soon",
+  now = "now",
+  past = "past",
+}
+
+interface ProposalBadgeProps {
+  type: ProposalBadgeType
+}
+const ProposalBadge = ({ type }: ProposalBadgeProps) => {
+  const badgeClasses = (() => {
+    switch (type) {
+      case ProposalBadgeType.now:
+        return "bg-[#FF0420] text-[#FBFCFE]"
+      case ProposalBadgeType.past:
+        return "bg-[#FF0420] text-[#FBFCFE]"
+      case ProposalBadgeType.soon:
+        return "bg-[#D6E4FF] text-[#0E4CAF]"
+      default:
+        return "bg-gray-200 text-gray-800" // Default styling
+    }
+  })()
+
+  return (
+    <div
+      className={`status-badge w-[46px] h-[24px] px-2 py-1 rounded-full flex gap-1 ${badgeClasses}`}
+    >
+      <div
+        className={
+          "w-[30px] h-[16px] text-xs font-medium leading-none text-center rounded-full flex items-center justify-center"
+        }
+      >
+        {type.toString()}
+      </div>
+    </div>
+  )
+}
+
+interface ProposalTextProps {
+  title: string
+  subtitle?: string
+}
+const ProposalTextContent = ({ title, subtitle }: ProposalTextProps) => {
+  return (
+    <div className="flex flex-col justify-center w-[42.1875rem] h-[48px] rounded-md">
+      <div className="text-base font-[500] text-[#0F111A] leading-normal">
+        {title}
+      </div>
+      <div className="text-base font-normal text-[#404454] leading-normal">
+        {subtitle}
+      </div>
+    </div>
+  )
+}
+
+interface ProposalDatesProps {
+  startDate: string
+  endDate: string
+}
+const ProposalDates = ({ startDate, endDate }: ProposalDatesProps) => {
+  return (
+    <div className="w-[187px] flex items-center justify-end">
+      <div className="text-base font-normal text-[#404454] whitespace-nowrap overflow-hidden text-right sm:text-sm">
+        {startDate} - {endDate}
+      </div>
+    </div>
+  )
+}
+
+interface ProposalArrowProps {
+  href: string
+}
+const ProposalArrow = ({ href }: ProposalArrowProps) => {
+  return (
+    <a href={href} className="block w-[36px] h-[36px]">
+      <div className="w-full h-full rounded-[6px] flex items-center justify-center p-[6px_12px_6px_12px] gap-[8px] bg-[#F2F3F8]">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9 5L16 12L9 19"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+    </a>
+  )
+}
+
+export { ProposalArrow, ProposalBadge, ProposalDates, ProposalTextContent }
+export default ProposalCard
