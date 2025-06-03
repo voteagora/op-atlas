b10bde7e35b8544be5e53f56e76965ec664bb0c6
daktic <garrett@voteagora.com>
2025-06-03 10:43:04 -0400

    Adding column styling

--- app/src/app/proposals/components/VotingSidebar/VotingColumn.tsx
+++ app/src/app/proposals/components/VotingSidebar/VotingColumn.tsx
@@ -1,5 +1,9 @@
 const VotingColumn = () => {
-  return <></>
+  return (
+    <div className="height-full border-l border-b border-r min-w-[19rem] h-[404px]">
+      Test
+    </div>
+  )
 }
 
 export default VotingColumn
