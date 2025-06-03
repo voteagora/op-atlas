39e9c7cdf8c83fdcc51fd35c3b476c6c86df1879
daktic <garrett@voteagora.com>
2025-06-03 15:11:02 -0400

    templatizing Canidate Card and Voiting column to fill in with data.

--- app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
+++ app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
@@ -1,9 +1,30 @@
 import CandidateCard from "@/app/proposals/components/VotingSidebar/votingColumn/CandidateCard"
 
+const MOCK_CANDIDATES = Array(8).fill({
+  name: "Username",
+  image: {
+    src: "https://i.imgur.com/0000000.png",
+    alt: "Image",
+  },
+  organizations: ["Org 1", "Org 2", "Org 3"],
+  buttonLink: "https://google.com",
+})
+
 const VotingColumn = () => {
   return (
-    <div className="height-full border-l border-b border-r min-w-[19rem] h-[404px]">
-      <CandidateCard />
+    <div className="w-[19rem] h-[25.25rem] pt-[1.5rem] pr-[1rem] pb-[1.5rem] pl-[1rem] gap-[var(--dimensions-8)] border-l border-b border-r rounded-b-[0.75rem]">
+      <div className="w-[17rem] h-[22.25rem] gap-4 flex flex-col">
+        <p className="pl-2 pr-2 h-5">8 Candidates</p>
+        {MOCK_CANDIDATES.map((candidate, idx) => (
+          <CandidateCard
+            key={idx}
+            img={candidate.image}
+            username={candidate.name}
+            organizations={candidate.organizations}
+            buttonLink={candidate.buttonLink}
+          />
+        ))}
+      </div>
     </div>
   )
 }
