c36558b5a83bef7801974ca2b10d3c30f861e489
daktic <garrett@voteagora.com>
2025-06-03 10:47:29 -0400

    import canidate card

--- app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
+++ app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
@@ -1,7 +1,9 @@
+import CandidateCard from "@/app/proposals/components/VotingSidebar/votingColumn/CandidateCard"
+
 const VotingColumn = () => {
   return (
     <div className="height-full border-l border-b border-r min-w-[19rem] h-[404px]">
-      Test
+      <CandidateCard />
     </div>
   )
 }
