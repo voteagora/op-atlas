1f23bfbe659f26dccde9a50adce3c95f609a210d
daktic <garrett@voteagora.com>
2025-06-03 15:35:59 -0400

    Added inner div and enforced outer div styling to bring it more inline with figma mock up

--- app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
+++ app/src/app/proposals/components/VotingSidebar/votingColumn/VotingColumn.tsx
@@ -12,18 +12,20 @@ const MOCK_CANDIDATES = Array(8).fill({
 
 const VotingColumn = () => {
   return (
-    <div className="w-[19rem] h-[25.25rem] pt-[1.5rem] pr-[1rem] pb-[1.5rem] pl-[1rem] gap-[var(--dimensions-8)] border-l border-b border-r rounded-b-[0.75rem]">
-      <div className="w-[17rem] h-[22.25rem] gap-4 flex flex-col">
+    <div className="w-[19rem] min-h-[25.25rem] pt-[1.5rem] pr-[1rem] pb-[1.5rem] pl-[1rem] gap-[var(--dimensions-8)] border-l border-b border-r rounded-b-[12px]">
+      <div className="w-[272px] h-[356px] gap-[16px] flex flex-col">
         <p className="pl-2 pr-2 h-5">8 Candidates</p>
-        {MOCK_CANDIDATES.map((candidate, idx) => (
-          <CandidateCard
-            key={idx}
-            img={candidate.image}
-            username={candidate.name}
-            organizations={candidate.organizations}
-            buttonLink={candidate.buttonLink}
-          />
-        ))}
+        <div className="w-full sm:w-[272px] h-[320px]">
+          {MOCK_CANDIDATES.map((candidate, idx) => (
+            <CandidateCard
+              key={idx}
+              img={candidate.image}
+              username={candidate.name}
+              organizations={candidate.organizations}
+              buttonLink={candidate.buttonLink}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
