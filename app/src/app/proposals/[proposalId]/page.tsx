13206f6477630f9c72871d8cad528bd5e9ff4e66
daktic <garrett@voteagora.com>
2025-06-03 15:57:59 -0400

    moved mock into page to simulate data flow

--- app/src/app/proposals/[proposalId]/page.tsx
+++ app/src/app/proposals/[proposalId]/page.tsx
@@ -1,10 +1,9 @@
 import { notFound } from "next/navigation"
 import Breadcrumbs from "@/app/proposals/components/Breadcrumbs"
-import ProposalContent from "@/app/proposals/components/proposalContent/ProposalContent"
+import ProposalTitle from "@/app/proposals/components/ProposalTitle"
+import ProposalStatus from "@/app/proposals/components/ProposalStatus"
+import ProposalDescription from "@/app/proposals/components/ProposalDescription"
 import VotingSidebar from "@/app/proposals/components/VotingSidebar/VotingSidebar"
-import ProposalHeader from "@/app/proposals/components/ProposalHeader"
-import { VotingCardProps } from "@/app/proposals/components/VotingSidebar/votingCard/VotingCard"
-import { ProposalType } from "@/lib/types"
 
 interface PageProps {
   params: {
@@ -12,103 +11,23 @@ interface PageProps {
   }
 }
 
-interface CardType {
-  signedIn: boolean
-  citizen: boolean
-  voted: boolean
-  votingOpen: boolean
-  votingComplete: boolean
-  startDate: Date
-  endDate: Date
-  proposalType: ProposalType
-}
-const CURRENT_DATE = new Date()
-
-const getVotingCardProps = (
-  cardType: CardType,
-): VotingCardProps | undefined => {
-  if (!cardType.signedIn) {
-    return {
-      cardText: {
-        title: "Cast your citizen vote",
-        description:
-          "This election uses approval voting, meaning voter can approve more than one candidate.",
-      },
-    }
-  }
-
-  const getOpenVotingTypes = (cardType: CardType) => {
-    if (cardType.voted) {
-      return {
-        cardText: {
-          title: "TODO",
-          description: "TODO",
-        },
-      }
-    }
-    return {
-      cardText: {
-        title: "TODO",
-        description: "TODO",
-      },
-    }
-  }
-
-  const getClosedVotingTypes = (cardType: CardType) => {
-    return {
-      cardText: {
-        title: "TODO",
-        description: "TODO",
-      },
-    }
-  }
-
-  const getCitizenTypes = (cardType: CardType) => {
-    if (cardType.votingOpen) {
-      return getOpenVotingTypes(cardType)
-    } else if (cardType.votingComplete) {
-      return getClosedVotingTypes(cardType)
-    }
-  }
-
-  const getNonCitizenTypes = (cardType: CardType) => {
-    return {
-      cardText: {
-        title: "TODO",
-        description: "TODO",
-      },
-    }
-  }
-
-  if (cardType.citizen) {
-    return getCitizenTypes(cardType)
-  }
-  return getNonCitizenTypes(cardType)
-}
-
 const Page = (params: PageProps) => {
   // Get the proposals page
 
   const { proposalId } = params.params
 
-  const proposalIdData = proposalId !== undefined
+  const proposalIdData = true
 
   if (!proposalIdData) {
     return notFound()
   }
 
   const MOCK = {
-    breadcrumbs: ["Proposals", "Audit Request Team"],
+    breadcrumbs: "Audit Request Team",
     title: "Developer Advisory Board: Audit Request Team",
     status: "Nominations [Start Date] - [End Date]",
     description:
-      "# Developer Advisory Board: Audit Request Team\n\n" +
-      "## Roles & Responsibilities\n\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n" +
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n" +
-      "* **Responsibility** for reviewing and auditing smart contracts\n" +
-      "* **Leadership** in maintaining security standards\n" +
-      "* **Collaboration** with development teams across projects",
+      "# Developer Advisory Board: Audit Request Team\n\nDescription",
     votingCardProps: {
       cardText: {
         title: "It's time to vote",
@@ -143,47 +62,25 @@ const Page = (params: PageProps) => {
     },
   }
 
-  const user = {
-    userId: "1",
-    citizenId: "1",
-  }
-
-  const userSignedIn = user.userId !== undefined
-  const userCitizen = user.citizenId !== undefined
-  const voted = true
-  const votingOpen = true
-  const votingComplete = false
-  const startDate = CURRENT_DATE
-  const endDate = CURRENT_DATE
-  const pType = "STANDARD" as ProposalType
-
-  const votingCardType = {
-    signedIn: userSignedIn,
-    citizen: userCitizen,
-    voted: voted,
-    votingOpen: votingOpen,
-    votingComplete: votingComplete,
-    startDate: startDate,
-    endDate: endDate,
-    proposalType: pType,
-  }
-  const votingCardData = getVotingCardProps(votingCardType)
-
   return (
-    <main className="flex w-full h-full pb-[160px] gap-[80px] mx-auto items-center">
-      <div className="flex flex-col w-2/3 mt-24 h-[865px] gap-[48px] mx-auto">
-        <div className="flex flex-col gap-[44px]">
-          <div className="flex justify-between items-start flex-col md:flex-row">
-            <div className="w-full flex flex-col gap-[44px] mb-8 md:mb-0">
-              <Breadcrumbs values={MOCK.breadcrumbs} />
-              <ProposalHeader title={MOCK.title} status={MOCK.status} />
-              <ProposalContent description={MOCK.description} />
+    <main className="flex flex-col flex-1 h-full items-center bg-secondary pb-20 md:pb-40 pt-10 md:pt-20">
+      <div className="w-full max-w-[1064px] min-h-[865px] px-4 md:px-0">
+        <div className="flex flex-col">
+          <div className="flex gap-8 lg:gap-16 justify-between items-start max-w-[76rem] flex-col md:flex-row md:items-start md:justify-between">
+            <div className="w-full md:w-body flex flex-col space-y-4 mb-8 md:mb-0">
+              <Breadcrumbs value={MOCK.breadcrumbs} />
+              <ProposalTitle title={MOCK.title} className="mt-2" />
+              <ProposalStatus status={MOCK.status} className="mt-2" />
+              <ProposalDescription
+                description={MOCK.description}
+                className="mt-4"
+              />
             </div>
             <div className="w-full md:w-[304px] md:ml-12">
               <VotingSidebar
                 className="sticky top-4"
                 votingColumnProps={MOCK.votingColumnProps}
-                votingCardProps={votingCardData!}
+                votingCardProps={MOCK.votingCardProps}
               />
             </div>
           </div>
