580992093740552fb6f0a88163949c5f77b5b760
daktic <garrett@voteagora.com>
2025-06-03 17:35:51 -0400

    Did some reorganizing and work on the proposals section to display content.

--- app/src/app/proposals/components/Breadcrumbs.tsx
+++ app/src/app/proposals/components/Breadcrumbs.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import { ChevronRight } from "lucide-react"
+import React from "react"
 
 interface BreadcrumbsProps {
-  value: string;
-  className?: string;
+  values: string[]
+  className?: string
 }
 
-const Breadcrumbs = ({ value, className = '' }: BreadcrumbsProps) => (
-  <div className={`flex gap-4 ${className}`}>{value}</div>
-);
+const Breadcrumbs = ({ values, className = "" }: BreadcrumbsProps) => (
+  <div className={`flex items-center ${className}`}>
+    {values.map((value, index) => (
+      <React.Fragment key={index}>
+        {index > 0 && <ChevronRight size={16} width={16} height={16} />}
+        <Breadcrumb value={value} index={index} />
+      </React.Fragment>
+    ))}
+  </div>
+)
 
-export default Breadcrumbs;
+const Breadcrumb = ({ value, index }: { value: string; index: number }) => (
+  <p
+    className={`font-normal text-[14px] leading-[20px] tracking-[0%] ${
+      index === 0 ? "muted-foreground" : "foreground"
+    }`}
+  >
+    {value}
+  </p>
+)
+
+export default Breadcrumbs
