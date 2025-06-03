3e9dcdf5b4298bc0535edcf722ffd4799c99bb15
daktic <garrett@voteagora.com>
2025-06-03 15:35:59 -0400

    Added inner div and enforced outer div styling to bring it more inline with figma mock up

--- app/src/app/proposals/components/VotingSidebar/VotingSidebar.tsx
+++ app/src/app/proposals/components/VotingSidebar/VotingSidebar.tsx
@@ -2,32 +2,22 @@ import React from "react"
 import VotingCard, {
   VotingCardProps,
 } from "@/app/proposals/components/VotingSidebar/votingCard/VotingCard"
-import VotingColumn, {
-  VotingColumnProps,
-} from "@/app/proposals/components/VotingSidebar/votingColumn/VotingColumn"
-import VotingRedirect, {
-  VotingRedirectProps,
-} from "@/app/proposals/components/VotingSidebar/VotingRedirect"
+import VotingColumn from "@/app/proposals/components/VotingSidebar/votingColumn/VotingColumn"
+import VotingRedirect from "@/app/proposals/components/VotingSidebar/VotingRedirect"
 
 interface VotingSidebarProps {
   className?: string
   votingCardProps: VotingCardProps
-  votingColumnProps: VotingColumnProps
-  votingRedirectProps?: VotingRedirectProps
 }
 
 const VotingSidebar = ({
   className = "",
   votingCardProps,
-  votingColumnProps,
-  votingRedirectProps,
 }: VotingSidebarProps) => (
-  <div className={`w-[304px] h-[792px] gap-6 flex flex-col ${className}`}>
-    <div className="w-[304px] h-[648px]">
-      <VotingCard {...votingCardProps} />
-      <VotingColumn {...votingColumnProps} />
-    </div>
-    {votingRedirectProps && <VotingRedirect {...votingRedirectProps} />}
+  <div className={`w-[304px] h-[792px] flex flex-col ${className}`}>
+    <VotingCard {...votingCardProps} />
+    <VotingColumn />
+    <VotingRedirect />
   </div>
 )
 
