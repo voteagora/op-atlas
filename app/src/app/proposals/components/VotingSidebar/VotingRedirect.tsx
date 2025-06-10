41744fe14c78e8b4a0e197d1457b33492995220f
daktic <garrett@voteagora.com>
2025-06-10 10:05:42 -0400

    Rebuilt the votingRedirectProps and components

--- app/src/app/proposals/components/VotingSidebar/VotingRedirect.tsx
+++ app/src/app/proposals/components/VotingSidebar/VotingRedirect.tsx
@@ -1,21 +1,27 @@
 export interface VotingRedirectProps {
-  title: string
-  subtext: string | React.ReactElement
+  callout: string
+  link?: {
+    linkText: string
+    linkHref: string
+  }
 }
 
-const VotingRedirect = ({ title, subtext }: VotingRedirectProps) => {
+const VotingRedirect = ({ callout, link }: VotingRedirectProps) => {
   return (
-    <div className="delegate-redirect-card w-[304px] h-[120px] gap-[var(--dimensions-7)] p-[var(--dimensions-8)] rounded-[12px] flex items-center justify-center bg-secondary">
-      <div className="w-[256px] h-[72px] gap-[var(--dimensions-5)] flex flex-col items-center justify-center">
-        <p className="w-[256px] h-[24px] font-['Inter'] font-semibold text-[16px] leading-[24px] text-center align-middle bg-foreground">
-          {title}
+    <div className="delegate-redirect-card w-[304px] h-[40px] pt-[10px] pr-[var(--dimensions-8)] pb-[10px] pl-[var(--dimensions-8)] gap-[var(--dimensions-7)] rounded-[6px] flex items-center justify-center bg-secondary min-mar">
+      <div className="flex items-center gap-[var(--dimensions-5)]">
+        <p className="font-['Inter'] font-[500] text-[14px] leading-[20px] tracking-[0%] text-center">
+          {callout}
         </p>
-        {typeof subtext === "string" ? (
-          <p className="w-[256px] h-[40px] font-['Inter'] font-normal text-[14px] leading-[20px] text-center underline decoration-solid decoration-[0%] underline-offset-[0%] bg-secondary-foreground">
-            {subtext}
-          </p>
-        ) : (
-          subtext
+        {link && (
+          <span className="font-['Inter'] font-[400] text-[14px] leading-[20px] tracking-[0%] text-center ml-2">
+            <a
+              className="underline decoration-solid underline-offset-[0%] decoration-[0%] color-[var(--secondary-foreground,#404454)]"
+              href={link.linkHref}
+            >
+              {link.linkText}
+            </a>
+          </span>
         )}
       </div>
     </div>
