e97f8e47263b1956052134f5ec7f483334f42d70
andrei <andrei.taraschuk@gmail.com>
2025-06-10 10:02:44 -0600

    Fixed footer

--- app/src/app/citizenship/components/ActiveCitizen.tsx
+++ app/src/app/citizenship/components/ActiveCitizen.tsx
@@ -1,22 +1,41 @@
 "use client"
 
-import { User } from "@prisma/client"
+import Link from "next/link"
 
 import { UserAvatarLarge } from "@/components/common/UserAvatarLarge"
 import { CheckboxCircleFIll } from "@/components/icons/reminx"
 import { AvatarBadge } from "@/components/ui/avatar"
+import { UserWithEmails } from "@/lib/types"
 
-export const ActiveCitizen = ({ user }: { user: User }) => {
+export const ActiveCitizen = ({ user }: { user: UserWithEmails }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full w-full gap-8">
+    <div className="flex flex-col items-center justify-center h-full gap-8 w-full">
       <UserAvatarLarge imageUrl={user.imageUrl}>
         <AvatarBadge className="absolute w-[20px] h-[20px] top-[20px] right-0 bg-white rounded-full">
           <CheckboxCircleFIll className="w-[20px] h-[20px]" fill="#FF0000" />
         </AvatarBadge>
       </UserAvatarLarge>
-      <div className="text-xl font-semibold">Welcome, Citizen!</div>
-      <div className="text-center">
-        {`You are now a member of the Citizens' House. Your votes help shape the future of the Optimism Collective.`}
+      <div className="flex flex-col items-center justify-center gap-12 max-w-[712px]">
+        <div className="text-xl font-semibold">Welcome, Citizen!</div>
+        <div className="text-center text-lg">
+          You are now a member of the Citizens&apos; House. Your votes help
+          shape the future of the Optimism Collective.
+        </div>
+        <div className="text-sm text-muted-foreground text-center">
+          Join the{" "}
+          <Link
+            href="https://t.me/citizenshouseannouncements"
+            target="_blank"
+            className="underline"
+          >
+            Citizens&apos; House Announcements Group
+          </Link>{" "}
+          on Telegram for important messages. We&apos;ll send emails to{" "}
+          <span className="font-semibold text-red-500">
+            {user.emails[0].email}
+          </span>{" "}
+          for future votes.
+        </div>
       </div>
     </div>
   )
