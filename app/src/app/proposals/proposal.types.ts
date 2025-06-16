693e871964f5c5467a9159eba12ea303da438051
daktic <garrett@voteagora.com>
2025-06-16 15:52:20 -0400

    Added proposalStatus to voteEnded cardText

--- app/src/app/proposals/proposal.types.ts
+++ app/src/app/proposals/proposal.types.ts
@@ -12,6 +12,7 @@ export interface ProposalPageDataInterface {
   endDate: Date
   proposalType: ProposalType
   proposalId: string
+  proposalStatus: string
   citizenEligibility: CitizenshipQualification | null
 }
 
