e38d0884b9d2b1b41a3fe696731301f94d2d6ec6
andrei <andrei.taraschuk@gmail.com>
2025-06-17 16:21:13 -0600

    Added S7 Citizenship to application requirements (#1024)

---
+++ app/src/hooks/citizen/useS7Citizen.ts
@@ -0,0 +1,33 @@
+"use client"
+
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+
+import { isS7Citizen } from "@/lib/actions/citizens"
+
+export const IS_S7_CITIZEN_QUERY_KEY = "is-s7-citizen"
+
+export const useIsS7Citizen = ({
+  id,
+  enabled = true,
+}: {
+  id: string
+  enabled?: boolean
+}) => {
+  const queryClient = useQueryClient()
+
+  const { data, isLoading, isSuccess, isError } = useQuery({
+    queryKey: [IS_S7_CITIZEN_QUERY_KEY, id],
+    queryFn: async () => {
+      return await isS7Citizen(id)
+    },
+    enabled,
+  })
+
+  const invalidate = () => {
+    return queryClient.invalidateQueries({
+      queryKey: [IS_S7_CITIZEN_QUERY_KEY, id],
+    })
+  }
+
+  return { data, isLoading, isSuccess, isError, invalidate }
+}
