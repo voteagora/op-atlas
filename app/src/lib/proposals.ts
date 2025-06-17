01e95e02055f2edc68ffec01f0c18d877d37232d
daktic <garrett@voteagora.com>
2025-06-17 11:51:08 -0400

    Adding local types. might be better elsewhere...

--- app/src/lib/proposals.ts
+++ app/src/lib/proposals.ts
@@ -1,22 +1,58 @@
 "use server"
+import { OffchainVote } from "@/app/proposals/proposal.types"
 import { ProposalBadgeType } from "@/app/proposals/proposalsPage/components/ProposalCard"
-import {
-  OffChainProposalResponse,
-  UIProposal,
-} from "@/app/api/agora/proposals/route"
-import { string } from "zod"
 import { getCitizenByType, getCitizenProposalVote } from "@/db/citizens"
 
-interface OffchainVote {
-  attestationId: string
-  voterAddress: string
-  proposalId: string
-  vote: { vote: string[] }
-  transactionHash?: string
-  citizenId: number
-  citizenCategory: string
-  createdAt: Date
-  updatedAt: Date
+export type OffChainProposalResponse = {
+  meta: {
+    has_next: boolean
+    total_returned: number
+    next_offset: number
+  }
+  data: OffChainProposal[]
+}
+
+export type OffChainProposal = {
+  id: string
+  proposer: string
+  snapshotBlockNumber: number
+  createdTime: string
+  startTime: string
+  startBlock: string
+  endTime: string
+  endBlock: string
+  cancelledTime: string | null
+  executedTime: string | null
+  executedBlock: string | null
+  queuedTime: string | null
+  markdowntitle: string
+  description: string
+  quorum: string
+  proposalData: object // We can define this more specifically if needed
+  proposalResults: object // We can define this more specifically if needed
+  proposalType: string
+  status: "PENDING" | "ACTIVE" | "CANCELLED" | "EXECUTED" | "QUEUED" | "FAILED"
+}
+
+// For UI
+export type UIProposal = {
+  id: string
+  badge: {
+    badgeType: ProposalBadgeType
+  }
+  voted?: boolean
+  passed?: boolean
+  textContent: {
+    title: string
+    subtitle?: string
+  }
+  dates: {
+    startDate: string
+    endDate: string
+  }
+  arrow: {
+    href: string
+  }
 }
 
 const getStandardProposlas = async () => {
