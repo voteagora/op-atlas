3d8f30e2acec4b7a497dfb05449e7f4c9d03d1f8
daktic <garrett@voteagora.com>
2025-06-18 09:17:04 -0400

    made URL ENV var

--- app/src/lib/proposals.ts
+++ app/src/lib/proposals.ts
@@ -57,7 +57,7 @@ export type UIProposal = {
 
 const getStandardProposlas = async () => {
   const response = await fetch(
-    `https://vote.optimism.io/api/v1/proposals?type=OFFCHAIN`,
+    `${process.env.NEXT_PUBLIC_AGORA_API_URL}/api/v1/proposals?type=OFFCHAIN`,
     {
       headers: {
         Authorization: `Bearer ${process.env.AGORA_API_KEY}`,
