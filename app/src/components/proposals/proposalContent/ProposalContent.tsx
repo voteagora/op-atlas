85151316ba9523cfe16245d29e46d34e9341c7a1
daktic <garrett@voteagora.com>
2025-06-20 13:47:33 -0400

    replaced Markdown from agora-next with simple markdown from react-markdown. removed

--- app/src/components/proposals/proposalContent/ProposalContent.tsx
+++ app/src/components/proposals/proposalContent/ProposalContent.tsx
@@ -1,32 +1,12 @@
 import React from "react"
-import styles from "@/app/proposals/proposalPage.module.scss"
-import Markdown from "@/components/common/Markdown/Markdown"
-
-function stripTitleFromDescription(title: string, description: string) {
-  if (description.startsWith(`# ${title}`)) {
-    const newDescription = description.slice(`# ${title}`.length).trim()
-    return newDescription
-  }
-  return description
-}
+import ReactMarkdown from "react-markdown"
 
 interface ProposalContentProps {
   description: string
-  className?: string
 }
 
-const ProposalContent = ({
-  description,
-  className = "",
-}: ProposalContentProps) => (
-  <div className={`${styles.proposal_description_md} ${className}`}>
-    <Markdown
-      content={stripTitleFromDescription(
-        description.split("\n")[0].replace("# ", ""),
-        description,
-      )}
-    />
-  </div>
+const ProposalContent = ({ description }: ProposalContentProps) => (
+  <ReactMarkdown>{description}</ReactMarkdown>
 )
 
 export default ProposalContent
