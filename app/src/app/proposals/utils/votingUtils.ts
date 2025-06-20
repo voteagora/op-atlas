b86e8e1537ee6b9fc83758b0234befbe0fab0ccf
daktic <garrett@voteagora.com>
2025-06-20 10:06:11 -0400

    returning empty data for now instead of mock data

--- app/src/app/proposals/utils/votingUtils.ts
+++ app/src/app/proposals/utils/votingUtils.ts
@@ -144,15 +144,17 @@ const getNonCitizenTypes = (proposalData: ProposalPageDataInterface) => {
 
 // Function to get vote options
 const getVoteOptions = () => {
-  return Array(8).fill({
-    name: "Username",
-    image: {
-      src: "https://i.imgur.com/0000000.png",
-      alt: "Image",
-    },
-    organizations: ["Org 1", "Org 2", "Org 3"],
-    buttonLink: "https://google.com",
-  })
+  // TODO: For approval voting
+  // return Array(8).fill({
+  //   name: "Username",
+  //   image: {
+  //     src: "https://i.imgur.com/0000000.png",
+  //     alt: "Image",
+  //   },
+  //   organizations: ["Org 1", "Org 2", "Org 3"],
+  //   buttonLink: "https://google.com",
+  // })
+  return []
 }
 
 /**
