51823a026fc784e4ec98c625ee399c8491775fc9
Garrett Berg <58493066+Daktic@users.noreply.github.com>
2025-06-25 12:22:50 -0400

    1084-p0---subtext-under-proposal-title-on-the-governance-page-should-read-voters-citizens-delegates-the-comma-should-be-a-colon (#1090)
    * temp dissable governance tab
    
    * Removing s7 citizenship requirement (#1087)
    
    * Updated proposals voters subtitle
    
    ---------
    
    Co-authored-by: stepandel <stepandel@gmail.com>
    Co-authored-by: andrei <andrei.taraschuk@gmail.com>
    Co-authored-by: daktic <garrett@voteagora.com>

--- app/src/components/common/Navbar.tsx
+++ app/src/components/common/Navbar.tsx
@@ -159,7 +159,7 @@ export const Navbar = () => {
                     </Link>
                   </div>
                 </div>
-                <div
+                {/* <div
                   className={cn(
                     "hidden sm:flex group gap-10 font-semibold text-text-muted h-full self-stretch hover:border-b-4 hover:border-[#0F111A] hover:text-text-default",
                     isGovernance
@@ -177,7 +177,7 @@ export const Navbar = () => {
                       Governance
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 <DropdownMenu>
                   <DropdownMenuTrigger className="h-full focus:outline-none focus:opacity-80">
                     <div className="hidden sm:flex group gap-10 font-semibold text-text-muted h-full self-stretch hover:border-b-4 hover:border-bg-tertiary hover:text-text-default">
