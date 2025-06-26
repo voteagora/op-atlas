48817d5bbd313bf85221c9de6df5e6740dd46bb4
stepandel <stepandel@gmail.com>
2025-06-25 22:11:43 -0700

    use functional color selection

--- app/src/components/proposals/proposalPage/VotingSidebar/votingCard/MyVote.tsx
+++ app/src/components/proposals/proposalPage/VotingSidebar/votingCard/MyVote.tsx
@@ -1,25 +1,32 @@
 import { VoteType } from "@/components/proposals/proposal.types"
 
 const MyVote = ({ voteType }: { voteType: VoteType }) => {
-  let boxColor = ""
-  let textColor = ""
-
-  switch (voteType) {
-    case VoteType.For:
-      boxColor = "bg-success"
-      textColor = "text-success-foreground"
-      break
-    case VoteType.Against:
-      boxColor = "bg-red-200"
-      textColor = "text-red-600"
-      break
-    case VoteType.Abstain:
-      boxColor = "bg-backgroundSecondary"
-      textColor = "text-primary"
-      break
-    default:
-      break
+  const getVoteColors = (voteType: VoteType) => {
+    switch (voteType) {
+      case VoteType.For:
+        return {
+          boxColor: "bg-success",
+          textColor: "text-success-foreground",
+        }
+      case VoteType.Against:
+        return {
+          boxColor: "bg-red-200",
+          textColor: "text-red-600",
+        }
+      case VoteType.Abstain:
+        return {
+          boxColor: "bg-backgroundSecondary",
+          textColor: "text-primary",
+        }
+      default:
+        return {
+          boxColor: "",
+          textColor: "",
+        }
+    }
   }
+
+  const { boxColor, textColor } = getVoteColors(voteType)
 
   return (
     <div className="w-[256px] h-[40px] gap-[8px]">
