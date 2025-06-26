69aa1e0c2f465f0ff7478b7cb7be8d04c254809c
stepandel <stepandel@gmail.com>
2025-06-26 12:48:30 -0700

    enable governance tab

--- app/src/components/common/Navbar.tsx
+++ app/src/components/common/Navbar.tsx
@@ -127,10 +127,10 @@ const Navbar = () => {
                     </Link>
                   </div>
                 </div>
-                {/* <div
+                <div
                   className={cn(
                     "hidden sm:flex group gap-10 font-semibold text-text-muted h-full self-stretch hover:border-b-4 hover:border-[#0F111A] hover:text-text-default",
-                    isProposals
+                    isGovernance
                       ? "border-b-4 border-[#0F111A] text-text-default"
                       : "",
                   )}
@@ -138,14 +138,14 @@ const Navbar = () => {
                   <div className="flex items-center">
                     <Link
                       className={`${
-                        isProposals ? "mt-1" : "group-hover:mt-1"
-                        } focus:outline-none focus:opacity-80`}
+                        isGovernance ? "mt-1" : "group-hover:mt-1"
+                      } focus:outline-none focus:opacity-80`}
                       href="/governance"
                     >
                       Governance
                     </Link>
                   </div>
-                </div> */}
+                </div>
                 <DropdownMenu>
                   <DropdownMenuTrigger className="h-full focus:outline-none focus:opacity-80">
                     <div className="hidden sm:flex group gap-10 font-semibold text-text-muted h-full self-stretch hover:border-b-4 hover:border-bg-tertiary hover:text-text-default">
