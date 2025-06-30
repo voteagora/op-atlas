10c4078e5f54a06fc5855ef1e3032f55e66e84c7
daktic <garrett@voteagora.com>
2025-06-30 14:44:26 -0400

    Added a variable Avatar size that could be swapped in other locations in a future update.

--- app/src/components/common/UserAvatarLarge.tsx
+++ app/src/components/common/UserAvatarLarge.tsx
@@ -5,6 +5,30 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar"
 interface Props {
   imageUrl?: string | null
   children?: React.ReactNode
+  size?: "sm" | "lg"
+}
+
+export const userAvatarSmall = ({ imageUrl, children }: Props) => {
+  return imageUrl ? (
+    <Avatar className="w-5 h-5">
+      <AvatarImage src={imageUrl} alt="avatar" />
+      {children}
+    </Avatar>
+  ) : (
+    <Avatar className="w-5 h-5">
+      <div
+        className={`w-5 h-5 my-0.5 flex items-center justify-center rounded-full border border-dashed border-muted bg-none`}
+      >
+        <Image
+          src="/assets/icons/user-icon.svg"
+          alt="user"
+          width={11}
+          height={11}
+        />
+      </div>
+      {children}
+    </Avatar>
+  )
 }
 
 export function UserAvatarLarge({ imageUrl, children }: Props) {
@@ -26,4 +50,15 @@ export function UserAvatarLarge({ imageUrl, children }: Props) {
       {children}
     </Avatar>
   )
+}
+
+export function UserAvatarVariable({ imageUrl, children, size }: Props) {
+  switch (size) {
+    case "sm":
+      return userAvatarSmall({ imageUrl, children, size })
+    case "lg":
+      return UserAvatarLarge({ imageUrl, children, size })
+    default:
+      return UserAvatarLarge({ imageUrl, children, size })
+  }
 }
