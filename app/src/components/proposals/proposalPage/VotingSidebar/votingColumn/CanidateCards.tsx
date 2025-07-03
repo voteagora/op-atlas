ed44a036c4cf400244c55b5f4a463dfd99d4716b
daktic <garrett@voteagora.com>
2025-07-03 16:56:26 -0400

    Added skeleton to candidate Cards

--- app/src/components/proposals/proposalPage/VotingSidebar/votingColumn/CanidateCards.tsx
+++ app/src/components/proposals/proposalPage/VotingSidebar/votingColumn/CanidateCards.tsx
@@ -3,6 +3,8 @@
 import CandidateCard from "@/components/proposals/proposalPage/VotingSidebar/votingColumn/CandidateCard"
 import { useMultipleUserQualifications } from "@/hooks/citizen/useMultiCitizenUser"
 import { VoteType } from "@/components/proposals/proposal.types"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useEffect, useState } from "react"
 
 interface CandidateCardsProps {
   userIds: string[]
@@ -10,12 +12,56 @@ interface CandidateCardsProps {
   setSelectedVote: (vote: { voteType: VoteType; selections?: number[] }) => void
 }
 
+const CandidateCardSkeleton = () => (
+  <div className="w-[272px] h-10 py-2 pr-[var(--dimensions-5)] pl-[var(--dimensions-5)] rounded-[6px]">
+    <div className="flex items-center h-5 gap-[8px] justify-between">
+      {/* Avatar skeleton */}
+      <Skeleton className="w-5 h-5 rounded-full" />
+
+      {/* Username skeleton */}
+      <Skeleton className="w-20 h-5" />
+
+      {/* Organizations skeleton */}
+      <Skeleton className="w-24 h-5" />
+
+      {/* Approval button skeleton */}
+      <Skeleton className="w-[65px] h-[24px] rounded-md" />
+    </div>
+  </div>
+)
+
 const CandidateCards = ({
   userIds,
   selectedVote,
   setSelectedVote,
 }: CandidateCardsProps) => {
-  const { data: candidates } = useMultipleUserQualifications(userIds)
+  const [isInitialLoad, setIsInitialLoad] = useState<boolean>(true)
+  const { data: candidates, isLoading: areCandidatesLoading } =
+    useMultipleUserQualifications(userIds)
+
+  useEffect(() => {
+    if (!areCandidatesLoading) {
+      const timer = setTimeout(() => {
+        setIsInitialLoad(false)
+      }, 150)
+      return () => clearTimeout(timer)
+    }
+  }, [areCandidatesLoading])
+
+  if (isInitialLoad) {
+    return (
+      <>
+        <div className="border-t px-2 align-left w-full">
+          <Skeleton className="h-5 w-24" />
+        </div>
+        <div className="w-full sm:w-[272px]">
+          {[...Array(3)].map((_, idx) => (
+            <CandidateCardSkeleton key={idx} />
+          ))}
+        </div>
+      </>
+    )
+  }
 
   if (!candidates) return null
 
