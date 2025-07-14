c1301c748b0f4c2c42340229ef5c7ef1d6dc3c15
andrei <andrei.taraschuk@gmail.com>
2025-07-14 16:07:53 -0600

    Increased username randomness. Resolved #1180 (#1212)

--- app/src/lib/utils/username.ts
+++ app/src/lib/utils/username.ts
@@ -1,10 +1,7 @@
 import { createHash } from "crypto"
 
 export function generateTemporaryUsername(id: string): string {
-    const suffix = createHash('sha256')
-        .update(id)
-        .digest('hex')
-        .slice(0, 4);
+  const suffix = createHash("sha256").update(id).digest("hex").slice(0, 8)
 
-    return `optimist-${suffix}`;
-} 
+  return `optimist-${suffix}`
+}
