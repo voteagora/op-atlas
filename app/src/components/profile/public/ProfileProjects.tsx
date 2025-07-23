93370846e9535da8314d325bbac55e0d74d07c43
daktic <garrett@voteagora.com>
2025-07-23 13:17:45 -0400

    removed check for applications. Will now render all projects

--- app/src/components/profile/public/ProfileProjects.tsx
+++ app/src/components/profile/public/ProfileProjects.tsx
@@ -15,31 +15,12 @@ function ProfileProjects({ projects }: { projects: ProjectWithDetailsLite[] }) {
     )
   }
 
-  // Projects exist
-  const projectsWithApplications = projects.filter(
-    (project) => project.applications && project.applications.length > 0
-  )
-
-  if (projectsWithApplications.length === 0) {
-    // No projects with applications
-    return (
-      <div className="flex flex-col space-y-4">
-        <h2 className="text-xl font-medium">Projects</h2>
-        <div className="flex flex-col space-y-4">
-          <p className="text-sm text-muted-foreground">
-            None of this user&#39;s projects have applied for funding
-          </p>
-        </div>
-      </div>
-    )
-  }
-
-  // Render projects with applications
+  // Render projects if they exist
   return (
     <div className="flex flex-col space-y-4">
       <h2 className="text-xl font-medium">Projects</h2>
       <div className="flex flex-col space-y-4">
-        {projectsWithApplications.map((project) => (
+        {projects.map((project) => (
           <ProjectWithLink key={project.id} project={project} />
         ))}
       </div>
