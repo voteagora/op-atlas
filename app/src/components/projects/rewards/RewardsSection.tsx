afba4f02995bd89358602d7504b95f123752551c
andrei <andrei.taraschuk@gmail.com>
2025-07-29 10:44:52 -0600

    No rewards view

--- app/src/components/projects/rewards/RewardsSection.tsx
+++ app/src/components/projects/rewards/RewardsSection.tsx
@@ -18,6 +18,10 @@ export function RewardsSection({
 }) {
   const isAdmin = useIsAdmin(team)
 
+  // If this project receives any grants, we’ll record it here.
+
+  const hasRewards = project.rewards.length > 0 || recurringRewards.length > 0
+
   const inProgressRewards = project.rewards.filter(
     (reward) =>
       reward.roundId !== "7" &&
@@ -34,55 +38,63 @@ export function RewardsSection({
       <div className="space-y-6">
         <h2 className="text-2xl text-text-default font-semibold">Rewards</h2>
         <p className="text-secondary-foreground text-base font-normal">
-          If this project receives any Retro Funding, we&apos;ll record it here.
+          {hasRewards
+            ? "Grant recipients have one year to claim their Retro Funding rewards. Unclaimed tokens will no longer be available after their expiration date."
+            : "If this project receives any Retro Funding, we'll record it here."}
         </p>
       </div>
-      <div className="flex flex-col space-y-4">
-        <h4 className="font-semibold text-text-default text-xl">In progress</h4>
-        {inProgressRewards.length || recurringRewards.length ? (
-          <ul className="space-y-3">
-            {recurringRewards.map((reward) => {
-              return (
-                <li key={reward.roundId}>
-                  <RecurringRewardAccordion
-                    reward={reward}
-                    isAdmin={Boolean(isAdmin)}
-                  />
-                </li>
-              )
-            })}
+      {hasRewards && (
+        <div>
+          <div className="flex flex-col space-y-4">
+            <h4 className="font-semibold text-text-default text-xl">
+              In progress
+            </h4>
+            {inProgressRewards.length || recurringRewards.length ? (
+              <ul className="space-y-3">
+                {recurringRewards.map((reward) => {
+                  return (
+                    <li key={reward.roundId}>
+                      <RecurringRewardAccordion
+                        reward={reward}
+                        isAdmin={Boolean(isAdmin)}
+                      />
+                    </li>
+                  )
+                })}
 
-            {inProgressRewards.map((reward) => (
-              <li key={reward.id}>
-                <RewardAccordion reward={reward} key={reward.id} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span className="text-sm text-muted-foreground px-3 py-2.5 rounded-md w-full border">
-            No in progress grants yet
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col space-y-4">
-        <h4 className="font-semibold text-text-default text-xl">Claimed</h4>
+                {inProgressRewards.map((reward) => (
+                  <li key={reward.id}>
+                    <RewardAccordion reward={reward} key={reward.id} />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <span className="text-sm text-muted-foreground px-3 py-2.5 rounded-md w-full border">
+                No in progress grants yet
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col space-y-4">
+            <h4 className="font-semibold text-text-default text-xl">Claimed</h4>
 
-        {/* TODO: Merge in claimed recurring rewards */}
+            {/* TODO: Merge in claimed recurring rewards */}
 
-        {claimedRewards.length ? (
-          <ul className="space-y-3">
-            {claimedRewards.map((reward) => (
-              <li key={reward.id}>
-                <RewardAccordion reward={reward} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span className="text-sm text-muted-foreground px-3 py-2.5 rounded-md w-full border">
-            No claimed grants yet
-          </span>
-        )}
-      </div>
+            {claimedRewards.length ? (
+              <ul className="space-y-3">
+                {claimedRewards.map((reward) => (
+                  <li key={reward.id}>
+                    <RewardAccordion reward={reward} />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <span className="text-sm text-muted-foreground px-3 py-2.5 rounded-md w-full border">
+                No claimed grants yet
+              </span>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
