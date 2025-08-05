454471ac4963a1df4b732526ec6af570c36ef47f
daktic <garrett@voteagora.com>
2025-08-05 17:43:33 -0400

    Fixed classnames not being applied to DropdownItem.tsx

--- app/src/components/common/DropdownItem.tsx
+++ app/src/components/common/DropdownItem.tsx
@@ -13,6 +13,7 @@ export default function DropdownItem({
     <div
       className={cn([
         "w-[394px] h-8 rounded-md hover:bg-secondary font-normal text-sm px-2 py-1.5",
+        className,
       ])}
     >
       {children}
