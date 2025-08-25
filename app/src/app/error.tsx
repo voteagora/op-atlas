ca11ec9e63d2533a52e6ad604a20813d9cdabbb8
daktic <garrett@voteagora.com>
2025-08-25 15:45:19 -0400

    Retry will refresh the page

--- app/src/app/error.tsx
+++ app/src/app/error.tsx
@@ -6,14 +6,21 @@ export default function Error() {
   return (
     <div className="flex flex-col items-center gap-6 h-app w-full justify-center lg:px-0 px-2">
       <h4 className="text-black font-semibold">
-        Oops, something went wrong. Please try again.
+        Something went wrong. Please try again.
       </h4>
 
       <div className="flex flex-wrap gap-3">
         <ExtendedLink
           as="button"
-          text={"Give Feedback"}
+          text={"Give feedback"}
           href={"https://ztfatrpxjq1.typeform.com/to/pBEaxkMN"}
+        />
+
+        <ExtendedLink
+          as="button"
+          text={"Retry"}
+          href={"#"}
+          onClick={() => window.location.reload()}
         />
       </div>
     </div>
