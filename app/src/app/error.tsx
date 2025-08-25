2d0a2435983138fee56147adfea0ee6aff31a649
daktic <garrett@voteagora.com>
2025-08-25 14:38:25 -0400

    Updates Copy with recommendations from Shaun

--- app/src/app/error.tsx
+++ app/src/app/error.tsx
@@ -1,42 +1,19 @@
 "use client"
 
-import Image from "next/image"
-
 import ExtendedLink from "@/components/common/ExtendedLink"
 
 export default function Error() {
   return (
     <div className="flex flex-col items-center gap-6 h-app w-full justify-center lg:px-0 px-2">
-      <Image
-        src={"/assets/icons/sunny-stars-custom-gimp-edit.svg"}
-        alt="Sunny Logo"
-        width={160}
-        height={160}
-      />
-
       <h4 className="text-black font-semibold">
-        Something went wrong. Please try again later.
+        Oops, something went wrong. Please try again.
       </h4>
 
       <div className="flex flex-wrap gap-3">
         <ExtendedLink
           as="button"
-          text={"Optimism"}
-          href={"https://optimism.io"}
-        />
-
-        <ExtendedLink as="button" text={"Retro Funding"} href={"/"} />
-
-        <ExtendedLink
-          as="button"
-          text={"Forum"}
-          href={"https://gov.optimism.io"}
-        />
-
-        <ExtendedLink
-          as="button"
-          text={"Delegates"}
-          href={"https://vote.optimism.io/delegates"}
+          text={"Give Feedback"}
+          href={"https://ztfatrpxjq1.typeform.com/to/pBEaxkMN"}
         />
       </div>
     </div>
