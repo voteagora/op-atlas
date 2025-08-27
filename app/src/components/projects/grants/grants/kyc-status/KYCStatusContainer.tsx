99887664d93cd3ac647bb5fd44b782ab7f8088e7
Lucas <lucasagfranca@gmail.com>
2025-08-26 23:10:59 -0300

    updated for prisma changes

--- app/src/components/projects/grants/grants/kyc-status/KYCStatusContainer.tsx
+++ app/src/components/projects/grants/grants/kyc-status/KYCStatusContainer.tsx
@@ -1,50 +1,15 @@
 "use client"
 
+import { KYCUser, Project } from "@prisma/client"
+import { useSession } from "next-auth/react"
+
 import GrantDeliveryAddress from "@/components/projects/grants/grants/kyc-status/GrantDeliveryAddress"
 import ProjectStatus from "@/components/projects/grants/grants/kyc-status/ProjectStatus"
 import IndividualStatuses from "@/components/projects/grants/grants/kyc-status/user-status/IndividualStatuses"
 import LegalEntities from "@/components/projects/grants/grants/kyc-status/user-status/LegalEntities"
-import { KYCUserStatusProps, PersonaStatus } from "@/components/projects/types"
 import { useKYCProject } from "@/hooks/db/useKYCProject"
-import { KYCUser } from "@prisma/client"
-import { useSession } from "next-auth/react"
 
 const mockAddress = "0xc2658A2d5ADf4a4F08f5c9b83D39816951465538"
-
-const MOCK_USERS = [
-  {
-    name: "Shaun Lind",
-    email: "shaun@optimism.io",
-    organization: "Optimism Unlimited",
-    status: "pending" as PersonaStatus,
-  },
-  {
-    name: "Alex Johnson",
-    email: "alex@optimism.io",
-    organization: "Optimism Unlimited",
-    status: "completed" as PersonaStatus,
-    expirationDate: new Date("2026-08-11"),
-  },
-  {
-    name: "Jamie Smith",
-    email: "jamie@optimism.io",
-    organization: "Optimism Unlimited",
-    status: "needs_review" as PersonaStatus,
-  },
-  {
-    name: "Taylor Brown",
-    email: "taylor@optimism.io",
-    organization: "Optimism Unlimited",
-    status: "approved" as PersonaStatus,
-    expirationDate: new Date("2026-01-01"),
-  },
-  // {
-  //   name: "Morgan Lee",
-  //   email: "morgan@optimism.io",
-  //   organization: "Optimism Unlimited",
-  //   status: "declined" as PersonaStatus,
-  // },
-]
 
 const resolveProjectStatus = (users: Pick<KYCUser, "personaStatus">[]) => {
   // If any users are expired, failed, or declined, return "project_issue"
@@ -88,9 +53,13 @@ const resolveProjectStatus = (users: Pick<KYCUser, "personaStatus">[]) => {
   return "pending"
 }
 
-const KYCStatusContainer = ({ projectId }: { projectId: string }) => {
+const KYCStatusContainer = ({ project }: { project: Project }) => {
   const { data: session } = useSession()
-  const { data: kycUsers, isLoading, isError } = useKYCProject({ projectId })
+  const {
+    data: kycUsers,
+    isLoading,
+    isError,
+  } = useKYCProject({ projectId: project.id })
   const projectStatus = kycUsers ? resolveProjectStatus(kycUsers) : "pending"
 
   const handleEmailResend = (emailAddress: string) => {
@@ -112,7 +81,7 @@ const KYCStatusContainer = ({ projectId }: { projectId: string }) => {
     console.error("Error loading KYC users data")
   }
 
-  console.log({ users })
+  console.log({ users, project })
 
   return (
     <div className="flex flex-col max-w-[762px] gap-6">
@@ -129,7 +98,7 @@ const KYCStatusContainer = ({ projectId }: { projectId: string }) => {
         ) : (
           <>
             <ProjectStatus status={projectStatus} />
-            <GrantDeliveryAddress address={mockAddress} />
+            <GrantDeliveryAddress address={project.kycTeam.walletAddress} />
             {users && users.length > 0 && (
               <>
                 <IndividualStatuses users={users!.slice(0, -1)} />
