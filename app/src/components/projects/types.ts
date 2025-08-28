91d3b12c18f31388a4f8c9b1fdd7c3f94d3f3bb0
daktic <garrett@voteagora.com>
2025-08-28 18:40:14 -0400

    Added better email sending feedback

--- app/src/components/projects/types.ts
+++ app/src/components/projects/types.ts
@@ -13,9 +13,15 @@ export type PersonaStatus =
 // Useful to add project problems to persona status
 export type ExtendedPersonaStatus = PersonaStatus | "project_issue"
 
+export enum EmailState {
+  NOT_SENT = "NOT_SENT",
+  SENDING = "SENDING",
+  SENT = "SENT",
+}
 export interface KYCUserStatusProps {
   user: KYCUser
   isUser?: boolean
   handleEmailResend: (kycUser: KYCUser) => void
   emailResendBlock?: boolean
+  emailState: EmailState
 }
