7f7bb155b85f9c08f87e50fdcd8602cf70075abb
Sudheer <sudheer.105@gmail.com>
2025-08-29 18:56:54 +0530

    WIP

--- app/src/components/profile/public/ProfileHeaderLinks.tsx
+++ app/src/components/profile/public/ProfileHeaderLinks.tsx
@@ -1,12 +1,18 @@
-import { Agora, Discord, Farcaster, Github, Optimism } from "@/components/icons/socials"
+import {
+  Agora,
+  Discord,
+  Farcaster,
+  Github,
+  Optimism,
+} from "@/components/icons/socials"
 import useDelegateData from "@/hooks/api/useDelegateData"
 import { useFarcasterUserData } from "@/hooks/api/useFarcasterUserData"
 import { useGithubUserData } from "@/hooks/api/useGithubUserData"
 import { UserWithAddresses } from "@/lib/types"
 import { formatNumber } from "@/lib/utils"
-import ProfileSidebarLink from "./ProfileSidebarLink"
 import { truncateAddress } from "@/lib/utils/string"
 
+import ProfileSidebarLink from "./ProfileSidebarLink"
 
 export default function ProfileHeaderLinks({
   user,
@@ -14,21 +20,24 @@ export default function ProfileHeaderLinks({
   user: UserWithAddresses
 }) {
   const { delegate } = useDelegateData(user?.addresses?.map((a) => a.address))
-
-
-  const { user: farcasterUsers } = useFarcasterUserData(user?.farcasterId, !!user?.farcasterId)
+  const { user: farcasterUsers } = useFarcasterUserData(
+    user?.farcasterId,
+    !!user?.farcasterId,
+  )
   const { user: githubUserData } = useGithubUserData(user.github || "")
 
   return (
     <div className="flex flex-col">
       {/* Farcaster */}
-      {user.farcasterId &&
+      {user.farcasterId && (
         <ProfileSidebarLink
           href={`https://warpcast.com/${user.username}`}
           icon={<Farcaster className="w-[16px] h-[16px]" />}
           text={
-            <div className="flex gap-1">
-              <span className="text-sm text-black">@{user.username}</span>
+            <div className="flex gap-2">
+              <span className="text-sm text-black group-hover:underline">
+                @{user.username}
+              </span>
               {farcasterUsers?.users[0]?.follower_count && (
                 <>
                   <span className="text-sm text-gray-500 font-light">
@@ -42,7 +51,7 @@ export default function ProfileHeaderLinks({
             </div>
           }
         />
-      }
+      )}
 
       {/* Github */}
       {user.github && (
@@ -50,8 +59,10 @@ export default function ProfileHeaderLinks({
           href={`https://github.com/${user.github}`}
           icon={<Github className="w-[16px] h-[16px]" />}
           text={
-            <div className="flex gap-1">
-              <span className="text-sm text-black">@{user.github}</span>
+            <div className="flex gap-2">
+              <span className="text-sm text-black group-hover:underline">
+                @{user.github}
+              </span>
               {githubUserData?.followers && (
                 <>
                   <span className="text-sm text-gray-500 font-light">
@@ -73,11 +84,12 @@ export default function ProfileHeaderLinks({
           href={`https://discord.com/users/${user.discord}`}
           icon={<Discord className="w-[15px] h-[15px]" />}
           text={
-            <div className="flex gap-1">
-              <span className="text-sm text-black">@{user.discord}</span>
+            <div className="flex gap-2">
+              <span className="text-sm text-black group-hover:underline">
+                @{user.discord}
+              </span>
             </div>
           }
-
         />
       )}
 
@@ -88,7 +100,7 @@ export default function ProfileHeaderLinks({
           icon={<Agora className="w-[15px] h-[15px]" />}
           text={
             <span>
-              <span className="text-sm text-black">
+              <span className="text-sm text-black group-hover:underline">
                 {truncateAddress(delegate.address)}{" "}
               </span>
               <span className="text-gray-500 font-light">
@@ -102,21 +114,16 @@ export default function ProfileHeaderLinks({
         />
       )}
 
-
-
       {/* Gov Forum */}
       {user.govForumProfileUrl && (
         <ProfileSidebarLink
           href={user.govForumProfileUrl}
-          icon={
-            <Optimism className="w-[16px] h-[16px]" fill="#FF0000" />
-          }
+          icon={<Optimism className="w-[16px] h-[16px]" fill="#FF0000" />}
           text={
-            <span className="text-sm text-black">
+            <span className="text-sm text-black group-hover:underline">
               @{user.govForumProfileUrl.split("/u/")[1].split("/summary")[0]}
             </span>
           }
-
         />
       )}
     </div>
