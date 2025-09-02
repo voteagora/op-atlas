3ec0a14f5fe11ecb3dd709a92a47a7a1ed6e9423
daktic <garrett@voteagora.com>
2025-09-02 18:09:59 -0400

    We can now get associated projects using the org ID

--- app/src/components/projects/grants/grants/kyc-status/ConnctedOrganizationProjects.tsx
+++ app/src/components/projects/grants/grants/kyc-status/ConnctedOrganizationProjects.tsx
@@ -1,17 +1,60 @@
-import { Project } from "@prisma/client"
-
 import KYCSubSection from "@/components/projects/grants/grants/kyc-status/KYCSubSection"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useOrganization } from "@/hooks/db/useOrganization"
+import { useProject } from "@/hooks/db/useProject"
 
 const ConnectedOrganizationProjects = ({
-  projects,
+  organizationId,
 }: {
-  projects: Project[]
+  organizationId: string
 }) => {
-  console.log({ projects })
+  const { data: organization, isLoading } = useOrganization({
+    id: organizationId,
+  })
+
+  console.log({ organization })
   return (
     <KYCSubSection title="Projects">
-      <div></div>
+      <div className="space-y-4">
+        {isLoading ? (
+          <Skeleton className="h-5 w-32" />
+        ) : organization?.projects.length === 0 ? (
+          <div>No projects found</div>
+        ) : (
+          organization?.projects.map((project) => (
+            <ProjectRow key={project.id} projectId={project.projectId} />
+          ))
+        )}
+      </div>
     </KYCSubSection>
+  )
+}
+
+const ProjectRow = ({ projectId }: { projectId: string }) => {
+  const { data: project } = useProject({ id: projectId })
+  console.log({ project })
+  if (!project) {
+    return null
+  }
+  return (
+    <div className="flex flex-row items-center justify-between py-2 px-3 gap-2 border rounded-md">
+      <div className="flex flex-row gap-2">
+        <div className="w-6 h-6">
+          {project.thumbnailUrl ? (
+            <img
+              src={project.thumbnailUrl}
+              alt={project.name}
+              className="w-full h-full object-cover rounded-sm"
+            />
+          ) : (
+            <div className="w-full h-full bg-gray-200 rounded-sm"></div>
+          )}
+        </div>
+        <p className="font-inter text-sm font-normal leading-5">
+          {project.name}
+        </p>
+      </div>
+    </div>
   )
 }
 
