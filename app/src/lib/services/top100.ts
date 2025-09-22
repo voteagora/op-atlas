dff9cd528460a4bd1aeaff20b8d6665b23a9bbe5
Sudheer <sudheer.105@gmail.com>
2025-09-22 22:10:24 +0530

    Add more addresses

--- app/src/lib/services/top100.ts
+++ app/src/lib/services/top100.ts
@@ -3,6 +3,9 @@ import { prisma } from "@/db/client"
 const DEV_TOP100_BYPASS = new Set([
   "0xbb8dbd9cc7ada9f4e31d4bd8c7a0410f2333c81a",
   "0x301987c30f12117f2c2e20bf46b7f420123e8068",
+  "0x47e7cee058e7e33da6ea2ba9ba7a14ae5d7e8cc4",
+  "0x4f9ccd8c2d017ead0cdaac6692c9bc96c92e53",
+  "0xb612723059eeebf5ca79b1ea58ef94cb3abb8a6",
 ])
 
 export async function isTop100Delegate(addresses: string[]): Promise<boolean> {
