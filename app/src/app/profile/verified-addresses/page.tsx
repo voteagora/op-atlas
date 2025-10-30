404e6302628e65c005ae9900c52eb37b768b84b6
Atomauro <42704921+atomauro@users.noreply.github.com>
2025-10-30 13:18:43 -0500

    spacing improvement by Shaun (#1444)

--- app/src/app/profile/verified-addresses/page.tsx
+++ app/src/app/profile/verified-addresses/page.tsx
@@ -20,30 +20,32 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col gap-12 text-secondary-foreground">
-      <h2 className="text-foreground text-2xl font-semibold">Linked Wallets</h2>
-      <div className="text-secondary-foreground">
-        Display your attestations, ENS, and more.
-        <br />
-        <br />
-        <ul className="list-disc list-outside pl-5">
-          <li>
-            If you&apos;re a citizen or guest voter, please link your
-            badgeholder address.
-          </li>
-          <li>
-            If you&apos;re a Token House delegate, please link your delegate
-            address.
-          </li>
-          <li>
-            If you&apos;ve received Foundation attestations, please link the
-            relevant addresses.
-          </li>
-          <li>
-            Set a governance address—this is where you&apos;ll receive
-            attestations, including the voting badge for citizens and guest
-            voters.
-          </li>
-        </ul>
+      <div className="flex flex-col gap-6">
+        <h2 className="text-foreground text-2xl font-semibold">Linked Wallets</h2>
+        <div className="text-secondary-foreground">
+          Display your attestations, ENS, and more.
+          <br />
+          <br />
+          <ul className="list-disc list-outside pl-5">
+            <li>
+              If you&apos;re a citizen or guest voter, please link your
+              badgeholder address.
+            </li>
+            <li>
+              If you&apos;re a Token House delegate, please link your delegate
+              address.
+            </li>
+            <li>
+              If you&apos;ve received Foundation attestations, please link the
+              relevant addresses.
+            </li>
+            <li>
+              Set a governance address—this is where you&apos;ll receive
+              attestations, including the voting badge for citizens and guest
+              voters.
+            </li>
+          </ul>
+        </div>
       </div>
 
       <VerifiedAddressesContent userId={session.user.id} />
