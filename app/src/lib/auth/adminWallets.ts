af6a0edf23c7d390b25b7bb71542529e7cc75de4
Lucas <lucasagfranca@gmail.com>
2025-11-13 20:27:16 -0300

    Add Shaun's wallet as admin for impersonation functionality

--- app/src/lib/auth/adminWallets.ts
+++ app/src/lib/auth/adminWallets.ts
@@ -12,4 +12,5 @@ export const ADMIN_WALLETS = [
   '0xfD4bBFB34ce81655a5EBf3fB18597d4BbA1087E2', // OP Sara
   '0x3584af1409456aF9dB967e46CB2dBc28adBd488c', // OP Emily
   '0xef297E3c5f5ABDEC0a520C743Db19417deb41a86', // OP Julian
+  '0x13ac7d7da4f9063ba7cabc2ad75f90afb3d0877b', // OP Shaun
 ].map(addr => addr.toLowerCase())
